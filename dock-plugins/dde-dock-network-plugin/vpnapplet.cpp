ad0b318eecfe8afafc9425ff1f497ee72e393caf
Hualet Wang <mr.asianwang@gmail.com>
2015-07-21 09:25:15 +0800

    bugfix: vpn enabled/disabled caused crash

--- dock-plugins/dde-dock-network-plugin/vpnapplet.cpp
+++ dock-plugins/dde-dock-network-plugin/vpnapplet.cpp
@@ -19,44 +19,44 @@ VPNApplet::VPNApplet(VPNComponent * host) :
 void VPNApplet::updateContent()
 {
     if (m_mainFrame) {
-        m_mainFrame->setParent(NULL);
         m_mainFrame->deleteLater();
-    } else {
-        m_mainFrame = new QFrame(this);
-        m_mainFrame->setFixedWidth(200);
-
-        QVBoxLayout * mainLayout = new QVBoxLayout(m_mainFrame);
-
-        QHBoxLayout * firstLineLayout = new QHBoxLayout;
-
-        QLabel * vpnLabel = new QLabel("VPN");
-        vpnLabel->setStyleSheet("QLabel { color: white }");
-
-        SwitchButton * vpnEnabledSwitch = new SwitchButton;
-        vpnEnabledSwitch->setChecked(m_host->m_enabled);
-
-        connect(vpnEnabledSwitch, &SwitchButton::toggled, [=](bool checked){
-            m_host->m_dbusNetwork->setVpnEnabled(checked);
-        });
-
-        firstLineLayout->addWidget(vpnLabel);
-        firstLineLayout->addStretch();
-        firstLineLayout->addWidget(vpnEnabledSwitch);
-
-        mainLayout->addLayout(firstLineLayout);
-
-        foreach (VPN vpn, m_host->m_vpns) {
-            QHBoxLayout * layout = new QHBoxLayout;
-
-            QLabel * vpnName = new QLabel(vpn.id);
-            vpnName->setStyleSheet("QLabel { color: white }");
-
-            layout->addWidget(vpnName);
-
-            mainLayout->addLayout(layout);
-        }
-
-        m_mainFrame->adjustSize();
-        this->adjustSize();
     }
+
+    m_mainFrame = new QFrame(this);
+    m_mainFrame->setFixedWidth(200);
+
+    QVBoxLayout * mainLayout = new QVBoxLayout(m_mainFrame);
+
+    QHBoxLayout * firstLineLayout = new QHBoxLayout;
+
+    QLabel * vpnLabel = new QLabel("VPN");
+    vpnLabel->setStyleSheet("QLabel { color: white }");
+
+    SwitchButton * vpnEnabledSwitch = new SwitchButton;
+    vpnEnabledSwitch->setChecked(m_host->m_enabled);
+
+    connect(vpnEnabledSwitch, &SwitchButton::toggled, [=](bool checked){
+        m_host->m_dbusNetwork->setVpnEnabled(checked);
+    });
+
+    firstLineLayout->addWidget(vpnLabel);
+    firstLineLayout->addStretch();
+    firstLineLayout->addWidget(vpnEnabledSwitch);
+
+    mainLayout->addLayout(firstLineLayout);
+
+    foreach (VPN vpn, m_host->m_vpns) {
+        QHBoxLayout * layout = new QHBoxLayout;
+
+        QLabel * vpnName = new QLabel(vpn.id);
+        vpnName->setStyleSheet("QLabel { color: white }");
+
+        layout->addWidget(vpnName);
+
+        mainLayout->addLayout(layout);
+    }
+
+    m_mainFrame->adjustSize();
+
+    this->adjustSize();
 }
