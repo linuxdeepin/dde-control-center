0e23c06b9d223a16ed810123ae82f3b9b2b007b0
zccrs <ccrr1314@live.com>
2015-08-05 18:08:25 +0800

    给Boot Menu添加颜色选取控件

--- modules/grub/grubbackground.cpp
+++ modules/grub/grubbackground.cpp
@@ -4,7 +4,7 @@
 
 GrubBackground::GrubBackground(QWidget *parent) : QWidget(parent)
 {
-    setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
     GrubDbus *dbus = GrubDbus::instance();
     updateBackground(dbus->background());
@@ -14,7 +14,7 @@ GrubBackground::GrubBackground(QWidget *parent) : QWidget(parent)
 void GrubBackground::paintEvent(QPaintEvent *e)
 {
     QPainter pa(this);
-    pa.drawPixmap(0, 0, m_background);
+    pa.drawPixmap(rect(), m_background);
 
     QWidget::paintEvent(e);
 }
@@ -23,7 +23,7 @@ bool GrubBackground::updateBackground(const QString &filename)
 {
     bool result = m_background.load(filename);
     if(result){
-        setFixedSize(m_background.size());
+        setMinimumSize(m_background.size());
         update();
     }
     return result;
--- modules/grub/grubwidget.cpp
+++ modules/grub/grubwidget.cpp
@@ -1,6 +1,7 @@
 #include "grubwidget.h"
 #include <libdui/dtextbutton.h>
 #include <libdui/dseparatorhorizontal.h>
+#include <libdui/dcolorpicker.h>
 #include <QPixmap>
 #include <QDebug>
 #include <QColorDialog>
@@ -27,6 +28,8 @@ GrubWidget::~GrubWidget()
 
 void GrubWidget::init()
 {
+    m_layout->setMargin(0);
+
     m_header->setTitle(tr("Boot Menu"));
     m_header->setContent(new DTextButton(tr("Reset")));
 
@@ -38,14 +41,30 @@ void GrubWidget::init()
     m_arrowBootDelay->setContent(new QWidget);
     m_arrowBootDelay->setExpand(false);
 
+    DColorPicker *picker1 = new DColorPicker(12, 19, 12, 2, 10, this);
+    picker1->addColorGradient(QPoint(1, 1), QPoint(12, 1), Qt::black, Qt::white);
+    picker1->addColorGradient(QPoint(1, 2), QPoint(6, 7), "#000011", "#00FFFF");
+    picker1->addColorGradient(QPoint(1, 8), QPoint(6, 13), "#330000", "#33FFFF");
+    picker1->addColorGradient(QPoint(1, 14), QPoint(6, 19), "#660000", "#66FFFF");
+    picker1->addColorGradient(QPoint(7, 2), QPoint(12, 7), "#990000", "#99FFFF");
+    picker1->addColorGradient(QPoint(7, 8), QPoint(12, 13), "#CC0000", "#CCFFFF");
+    picker1->addColorGradient(QPoint(7, 14), QPoint(12, 19), "#FF0000", "#FFFFFF");
     m_arrowTextColor->setTitle(tr("Text Color"));
-    m_arrowTextColor->setContent(new QColorDialog);
+    m_arrowTextColor->setContent(picker1);
     m_arrowTextColor->setExpand(false);
 
+    DColorPicker *picker2 = new DColorPicker(12, 19, 12, 2, 10, this);
+    picker2->addColorGradient(QPoint(1, 1), QPoint(12, 1), Qt::black, Qt::white);
+    picker2->addColorGradient(QPoint(1, 2), QPoint(6, 7), "#000011", "#00FFFF");
+    picker2->addColorGradient(QPoint(1, 8), QPoint(6, 13), "#330000", "#33FFFF");
+    picker2->addColorGradient(QPoint(1, 14), QPoint(6, 19), "#660000", "#66FFFF");
+    picker2->addColorGradient(QPoint(7, 2), QPoint(12, 7), "#990000", "#99FFFF");
+    picker2->addColorGradient(QPoint(7, 8), QPoint(12, 13), "#CC0000", "#CCFFFF");
+    picker2->addColorGradient(QPoint(7, 14), QPoint(12, 19), "#FF0000", "#FFFFFF");
     m_arrowSelectedTextColor->setTitle("Selected Text Color");
     QColorDialog *dialog = new QColorDialog;
     dialog->setOptions(QColorDialog::NoButtons);
-    m_arrowSelectedTextColor->setContent(dialog);
+    m_arrowSelectedTextColor->setContent(picker2);
     m_arrowSelectedTextColor->setExpand(false);
 
     m_layout->setSpacing(0);
