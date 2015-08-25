36179a26302ddafe9e6bcb81a93f570de1003eac
杨万青 <yangwanqing@linuxdeepin.com>
2015-08-25 14:11:53 +0800

    Account: update dbus interfaces

--- modules/account/dbus/dbussessionmanager.h
+++ modules/account/dbus/dbussessionmanager.h
@@ -36,7 +36,8 @@ class DBusSessionManager: public QDBusAbstractInterface
         if (interfaceName !="com.deepin.SessionManager")
             return;
         QVariantMap changedProps = qdbus_cast<QVariantMap>(arguments.at(1).value<QDBusArgument>());
-        foreach(const QString &prop, changedProps.keys()) {
+        QStringList keys = changedProps.keys();
+        foreach(const QString &prop, keys) {
         const QMetaObject* self = metaObject();
             for (int i=self->propertyOffset(); i < self->propertyCount(); ++i) {
                 QMetaProperty p = self->property(i);
