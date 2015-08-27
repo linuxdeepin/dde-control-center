18eef76c0baadb0f32d5462ad13a23aded8d04b1
石博文 <sbw@sbw.so>
2015-08-27 17:25:40 +0800

    moduleheader: add tips text
    Change-Id: Ic99a25b0aba78d1d86b1a2a5d253ea6af56ce1f8

--- widgets/moduleheader.cpp
+++ widgets/moduleheader.cpp
@@ -1,5 +1,6 @@
 #include <QLabel>
 #include <QDebug>
+#include <QTimer>
 
 #include <libdui/dthememanager.h>
 #include <libdui/dtextbutton.h>
@@ -7,7 +8,8 @@
 #include "moduleheader.h"
 
 ModuleHeader::ModuleHeader(QString title, bool addResetButton, QWidget * parent) :
-    DBaseLine(parent)
+    DBaseLine(parent),
+    m_timer(nullptr)
 {
     D_THEME_INIT_WIDGET(ModuleHeader);
 
@@ -17,10 +19,43 @@ ModuleHeader::ModuleHeader(QString title, bool addResetButton, QWidget * parent)
     this->setLeftContent(m_title);
 
     if (addResetButton) {
+        m_tipsLabel = new QLabel(tr("Reset Success"), this);
+        m_tipsLabel->setObjectName("TipsLabel");
         m_reset = new DTextButton("Reset", this);
-        this->setRightContent(m_reset);
+        m_timer = new QTimer;
 
+        QHBoxLayout *layout = new QHBoxLayout;
+        layout->addWidget(m_tipsLabel);
+        layout->addWidget(m_reset);
+
+        QWidget *widget = new QWidget;
+        widget->setLayout(layout);
+
+        setRightContent(widget);
+
+        m_timer->setInterval(800);
+
+        connect(m_timer, &QTimer::timeout, this, &ModuleHeader::resetUI);
         connect(m_reset, &DTextButton::clicked, this, &ModuleHeader::resetButtonClicked);
+        connect(m_reset, &DTextButton::clicked, [this] {
+            m_tipsLabel->show();
+            m_reset->hide();
+            m_timer->start();
+        });
+
+        resetUI();
     }
+}
+
+ModuleHeader::~ModuleHeader()
+{
+    if (m_timer)
+        m_timer->deleteLater();
+}
+
+void ModuleHeader::resetUI()
+{
+    m_tipsLabel->hide();
+    m_reset->show();
 }
 
--- widgets/moduleheader.h
+++ widgets/moduleheader.h
@@ -3,6 +3,9 @@
 
 #include <libdui/dbaseline.h>
 
+#include <QLabel>
+#include <QTimer>
+
 DUI_BEGIN_NAMESPACE
 class DTextButton;
 DUI_END_NAMESPACE
@@ -15,13 +18,19 @@ class ModuleHeader : public DBaseLine
     Q_OBJECT
 public:
     ModuleHeader(QString title, bool addResetButton = true, QWidget * parent = 0);
+    ~ModuleHeader();
 
 signals:
     void resetButtonClicked();
 
+private slots:
+    void resetUI();
+
 private:
     QLabel * m_title;
+    QLabel * m_tipsLabel;
     DTextButton * m_reset;
+    QTimer * m_timer;
 };
 
 #endif // MODULEHEADER_H
