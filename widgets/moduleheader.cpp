fcb618fea41f4f1d6560165e00ae4efecea70a5a
penghui <penghui@linuxdeepin.com>
2015-08-28 13:29:30 +0800

    moduleheader add a overload function
    the left content colud add custom widget in moduleheader
    
    Change-Id: Ib0e807572cd7ab8120cfa2bf990ac9720fde8e64

--- widgets/moduleheader.cpp
+++ widgets/moduleheader.cpp
@@ -1,6 +1,7 @@
 #include <QLabel>
 #include <QDebug>
 #include <QTimer>
+#include <QWidget>
 
 #include <libdui/dthememanager.h>
 #include <libdui/dtextbutton.h>
@@ -8,16 +9,38 @@
 #include "moduleheader.h"
 
 ModuleHeader::ModuleHeader(QString title, bool addResetButton, QWidget * parent) :
-    DBaseLine(parent),
-    m_timer(nullptr)
+    DBaseLine(parent)
 {
     D_THEME_INIT_WIDGET(ModuleHeader);
 
-    this->setFixedHeight(48);
-
     m_title = new QLabel(title, this);
     this->setLeftContent(m_title);
+    initUI(addResetButton);
+}
 
+ModuleHeader::ModuleHeader(QWidget *leftContent, bool addResetButton, QWidget *parent):
+      DBaseLine(parent)
+{
+    D_THEME_INIT_WIDGET(ModuleHeader);
+
+    this->setLeftContent(leftContent);
+    initUI(addResetButton);
+}
+ModuleHeader::~ModuleHeader()
+{
+}
+void ModuleHeader::initUI(bool addResetButton) {
+    this->setFixedHeight(48);
+    m_rightContentWidget = new RightContentWidget(addResetButton);
+
+    this->setRightContent(m_rightContentWidget);
+    connect(m_rightContentWidget, &RightContentWidget::resetButtonClicked,
+            this, &ModuleHeader::resetButtonClicked);
+}
+RightContentWidget::RightContentWidget(bool addResetButton, QWidget *parent):
+    QWidget(parent),
+    m_timer(nullptr)
+{
     if (addResetButton) {
         m_tipsLabel = new QLabel(tr("Reset Success"), this);
         m_tipsLabel->setObjectName("TipsLabel");
@@ -27,16 +50,11 @@ ModuleHeader::ModuleHeader(QString title, bool addResetButton, QWidget * parent)
         QHBoxLayout *layout = new QHBoxLayout;
         layout->addWidget(m_tipsLabel);
         layout->addWidget(m_reset);
-
-        QWidget *widget = new QWidget;
-        widget->setLayout(layout);
-
-        setRightContent(widget);
-
+        setLayout(layout);
         m_timer->setInterval(800);
 
-        connect(m_timer, &QTimer::timeout, this, &ModuleHeader::resetUI);
-        connect(m_reset, &DTextButton::clicked, this, &ModuleHeader::resetButtonClicked);
+        connect(m_timer, &QTimer::timeout, this, &RightContentWidget::resetUI);
+        connect(m_reset, &DTextButton::clicked, this, &RightContentWidget::resetButtonClicked);
         connect(m_reset, &DTextButton::clicked, [this] {
             m_tipsLabel->show();
             m_reset->hide();
@@ -46,16 +64,13 @@ ModuleHeader::ModuleHeader(QString title, bool addResetButton, QWidget * parent)
         resetUI();
     }
 }
-
-ModuleHeader::~ModuleHeader()
-{
-    if (m_timer)
-        m_timer->deleteLater();
-}
-
-void ModuleHeader::resetUI()
+void RightContentWidget::resetUI()
 {
     m_tipsLabel->hide();
     m_reset->show();
 }
-
+RightContentWidget::~RightContentWidget()
+{
+    if (m_timer)
+     m_timer->deleteLater();
+}
--- widgets/moduleheader.h
+++ widgets/moduleheader.h
@@ -13,24 +13,37 @@ DUI_END_NAMESPACE
 DUI_USE_NAMESPACE
 
 class QLabel;
+class RightContentWidget;
 class ModuleHeader : public DBaseLine
 {
     Q_OBJECT
 public:
     ModuleHeader(QString title, bool addResetButton = true, QWidget * parent = 0);
+    ModuleHeader(QWidget* leftContent, bool addResetButton = true, QWidget *parent=0);
     ~ModuleHeader();
+public slots:
+    void initUI(bool addResetButton);
+signals:
+    void resetButtonClicked();
+private:
+    RightContentWidget* m_rightContentWidget;
+    QLabel * m_title;
+};
 
+class RightContentWidget: public QWidget
+{
+    Q_OBJECT
+public:
+    RightContentWidget(bool addResetButton, QWidget *parent=0);
+    ~RightContentWidget();
 signals:
     void resetButtonClicked();
 
 private slots:
     void resetUI();
-
 private:
-    QLabel * m_title;
     QLabel * m_tipsLabel;
     DTextButton * m_reset;
     QTimer * m_timer;
 };
-
 #endif // MODULEHEADER_H
