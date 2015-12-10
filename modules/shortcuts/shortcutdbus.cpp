c71fcefb3505af8fb842aac51b53de930a387642
zccrs <ccrr1314@live.com>
2015-12-10 14:37:37 +0800

    Shortcuts: 新增切换窗口特效快捷键
    tower: https://tower.im/projects/715dbdd34f694ea08f6de7888b7ffd7d/todos/66898865823f40afa17d5aec6d6b0d70/
    
    Change-Id: Ic5d8e1fc76e9d381f92b20ba1fd82548b4124efa

--- modules/shortcuts/shortcutdbus.cpp
+++ modules/shortcuts/shortcutdbus.cpp
@@ -46,7 +46,8 @@ ShortcutDbus::ShortcutDbus(QObject *parent)
     list << "launcher" << "show-desktop" << "lock-screen" << "file-manager" << "show-dock" << "screenshot"
          << "screenshot-window" << "screenshot-delayed" << "terminal" << "terminal-quake"
          << "logout" << "switch-layout" << "preview-workspace" << "expose-windows" << "expose-all-windows"
-         << "switch-group" << "switch-group-backward" << "switch-applications" << "switch-applications-backward";
+         << "switch-group" << "switch-group-backward" << "switch-applications" << "switch-applications-backward"
+         << "wm-switcher";
 
     QDBusInterface dbus("com.deepin.daemon.InputDevice.InputDevices",
                         "/com/deepin/daemon/InputDevice/TouchPad",
