e6e22bc720b546463a16d1adf007ed124bcb32d8
Xu Fasheng <fasheng.xu@gmail.com>
2016-01-13 21:39:43 +0800

    Network: fix json string escaped issue
    Convert normal string to JSON escaped string with the code from Qt. Or
    it will be a wrong behavior if the string contains some special
    character like '\'. A reproduce way is to set the mobile plan for a
    modem device and user could not save the settings then.
    
    Change-Id: I634efdeb8f308578dc239a429f0ff7549fda6c0d

--- modules/network/networkbaseeditline.cpp
+++ modules/network/networkbaseeditline.cpp
@@ -15,6 +15,188 @@ static QStringList availableSections;
 static ErrorInfo errors;
 static QPointer<NetworkBaseEditLine> firstBaseEditLine;
 
+// Convert normal string to JSON escaped string. The code is copy from
+// qtbase/src/corelib/json/qjsonwriter.cpp
+
+struct QUtf8BaseTraits
+{
+    static const bool isTrusted = false;
+    static const bool allowNonCharacters = true;
+    static const bool skipAsciiHandling = false;
+    static const int Error = -1;
+    static const int EndOfString = -2;
+
+    static bool isValidCharacter(uint u)
+    { return int(u) >= 0; }
+
+    static void appendByte(uchar *&ptr, uchar b)
+    { *ptr++ = b; }
+
+    static uchar peekByte(const uchar *ptr, int n = 0)
+    { return ptr[n]; }
+
+    static qptrdiff availableBytes(const uchar *ptr, const uchar *end)
+    { return end - ptr; }
+
+    static void advanceByte(const uchar *&ptr, int n = 1)
+    { ptr += n; }
+
+    static void appendUtf16(ushort *&ptr, ushort uc)
+    { *ptr++ = uc; }
+
+    static void appendUcs4(ushort *&ptr, uint uc)
+    {
+        appendUtf16(ptr, QChar::highSurrogate(uc));
+        appendUtf16(ptr, QChar::lowSurrogate(uc));
+    }
+
+    static ushort peekUtf16(const ushort *ptr, int n = 0)
+    { return ptr[n]; }
+
+    static qptrdiff availableUtf16(const ushort *ptr, const ushort *end)
+    { return end - ptr; }
+
+    static void advanceUtf16(const ushort *&ptr, int n = 1)
+    { ptr += n; }
+
+    // it's possible to output to UCS-4 too
+    static void appendUtf16(uint *&ptr, ushort uc)
+    { *ptr++ = uc; }
+
+    static void appendUcs4(uint *&ptr, uint uc)
+    { *ptr++ = uc; }
+};
+
+static inline uchar hexdig(uint u)
+{
+    return (u < 0xa ? '0' + u : 'a' + u - 0xa);
+}
+
+/// returns 0 on success; errors can only happen if \a u is a surrogate:
+/// Error if \a u is a low surrogate;
+/// if \a u is a high surrogate, Error if the next isn't a low one,
+/// EndOfString if we run into the end of the string.
+template <typename Traits, typename OutputPtr, typename InputPtr> static inline
+int toUtf8(ushort u, OutputPtr &dst, InputPtr &src, InputPtr end)
+{
+    if (!Traits::skipAsciiHandling && u < 0x80) {
+        // U+0000 to U+007F (US-ASCII) - one byte
+        Traits::appendByte(dst, uchar(u));
+        return 0;
+    } else if (u < 0x0800) {
+        // U+0080 to U+07FF - two bytes
+        // first of two bytes
+        Traits::appendByte(dst, 0xc0 | uchar(u >> 6));
+    } else {
+        if (!QChar::isSurrogate(u)) {
+            // U+0800 to U+FFFF (except U+D800-U+DFFF) - three bytes
+            if (!Traits::allowNonCharacters && QChar::isNonCharacter(u))
+                return Traits::Error;
+
+            // first of three bytes
+            Traits::appendByte(dst, 0xe0 | uchar(u >> 12));
+        } else {
+            // U+10000 to U+10FFFF - four bytes
+            // need to get one extra codepoint
+            if (Traits::availableUtf16(src, end) == 0)
+                return Traits::EndOfString;
+
+            ushort low = Traits::peekUtf16(src);
+            if (!QChar::isHighSurrogate(u))
+                return Traits::Error;
+            if (!QChar::isLowSurrogate(low))
+                return Traits::Error;
+
+            Traits::advanceUtf16(src);
+            uint ucs4 = QChar::surrogateToUcs4(u, low);
+
+            if (!Traits::allowNonCharacters && QChar::isNonCharacter(ucs4))
+                return Traits::Error;
+
+            // first byte
+            Traits::appendByte(dst, 0xf0 | (uchar(ucs4 >> 18) & 0xf));
+
+            // second of four bytes
+            Traits::appendByte(dst, 0x80 | (uchar(ucs4 >> 12) & 0x3f));
+
+            // for the rest of the bytes
+            u = ushort(ucs4);
+        }
+
+        // second to last byte
+        Traits::appendByte(dst, 0x80 | (uchar(u >> 6) & 0x3f));
+    }
+
+    // last byte
+    Traits::appendByte(dst, 0x80 | (u & 0x3f));
+    return 0;
+}
+
+static QByteArray jsonEscapedString(const QString &s)
+{
+    const uchar replacement = '?';
+    QByteArray ba(s.length(), Qt::Uninitialized);
+
+    uchar *cursor = reinterpret_cast<uchar *>(const_cast<char *>(ba.constData()));
+    const uchar *ba_end = cursor + ba.length();
+    const ushort *src = reinterpret_cast<const ushort *>(s.constBegin());
+    const ushort *const end = reinterpret_cast<const ushort *>(s.constEnd());
+
+    while (src != end) {
+        if (cursor >= ba_end - 6) {
+            // ensure we have enough space
+            int pos = cursor - (const uchar *)ba.constData();
+            ba.resize(ba.size()*2);
+            cursor = (uchar *)ba.data() + pos;
+            ba_end = (const uchar *)ba.constData() + ba.length();
+        }
+
+        uint u = *src++;
+        if (u < 0x80) {
+            if (u < 0x20 || u == 0x22 || u == 0x5c) {
+                *cursor++ = '\\';
+                switch (u) {
+                case 0x22:
+                    *cursor++ = '"';
+                    break;
+                case 0x5c:
+                    *cursor++ = '\\';
+                    break;
+                case 0x8:
+                    *cursor++ = 'b';
+                    break;
+                case 0xc:
+                    *cursor++ = 'f';
+                    break;
+                case 0xa:
+                    *cursor++ = 'n';
+                    break;
+                case 0xd:
+                    *cursor++ = 'r';
+                    break;
+                case 0x9:
+                    *cursor++ = 't';
+                    break;
+                default:
+                    *cursor++ = 'u';
+                    *cursor++ = '0';
+                    *cursor++ = '0';
+                    *cursor++ = hexdig(u>>4);
+                    *cursor++ = hexdig(u & 0xf);
+               }
+            } else {
+                *cursor++ = (uchar)u;
+            }
+        } else {
+            if (toUtf8<QUtf8BaseTraits>(u, cursor, src, end) < 0)
+                *cursor++ = replacement;
+        }
+    }
+
+    ba.resize(cursor - (const uchar *)ba.constData());
+    return ba;
+}
+
 void updateAllEditLineVisible(NetworkBaseEditLine *editLine)
 {
     for(NetworkBaseEditLine *line : editLine->parent()->findChildren<NetworkBaseEditLine*>()) {
@@ -110,14 +292,16 @@ void NetworkBaseEditLine::setDBusKey(const QJsonValue &key)
     if (key != cacheValue() || isValueError() || setKeyAlways()) {
         QString json;
 
-        if(key.isString())
-            json.append("\"" + key.toString() + "\"");
-        else if(key.isDouble())
+        if(key.isString()) {
+            json.append("\"" + jsonEscapedString(key.toString()) + "\"");
+        } else if(key.isDouble()) {
             json.append(QString::number(key.toDouble(-1)));
-        else if(key.isBool())
+        } else if(key.isBool()) {
             json.append(key.toBool() ? "true" : "false");
+        }
         /// TODO 此处将key转换成json的value类型，由于Qt json目前并不支持此类型，故需手动转换
         /// 目前只处理了String int double bool类型，以后有新类型的需求记得在此处添加
+        qDebug() << "setDBusKey: " << json;
 
         setCacheValue(key);
 
