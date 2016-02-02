b8a615f799f42e10955dac707058a9dd72478ae8
石博文 <sbw@sbw.so>
2016-02-02 13:49:38 +0800

    change loading animation picture
    Change-Id: I095c31c2df2f2e06bd1daaafa9e1a1f29e0ef486

--- modules/system_info/updateprogress.cpp
+++ modules/system_info/updateprogress.cpp
@@ -7,13 +7,13 @@ UpdateProgress::UpdateProgress(QWidget *parent) :
 {
     QStringList seq;
     for (int i(0); i != 61; ++i)
-        seq.append(QString(":/images/loading/images/eLoading/eLoading_%1.png").arg(i, 2, 10, QChar('0')));
+        seq.append(QString(":/images/loading/images/eLoading/eLoading%1.png").arg(i, 2, 10, QChar('0')));
     for (int i(0); i != 30; ++i)
         seq.append(QString());
 
     m_picSeq = new DPictureSequenceView(this);
     m_picSeq->setPictureSequence(seq);
-    m_picSeq->setFixedSize(33, 33);
+    m_picSeq->setFixedSize(35, 35);
     m_picSeq->move(-2, -2);
 
     this->setProperty("backgroundColor", QColor(255, 255, 255, 200));
