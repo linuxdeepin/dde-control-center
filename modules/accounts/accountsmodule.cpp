e1042da47dec088d1ff98473cb4be1b1cbce5a84
石博文 <sbw@sbw.so>
2016-11-03 09:56:24 +0800

    construct model and worker at initialize func
    Change-Id: I1735ebf7cb39f2c1b9fc1230738d9b59bcb36c8a

--- modules/accounts/accountsmodule.cpp
+++ modules/accounts/accountsmodule.cpp
@@ -4,44 +4,36 @@
 AccountsModule::AccountsModule(FrameProxyInterface *frame, QObject *parent)
     : QObject(parent),
       ModuleInterface(frame),
-      m_userList(new UserModel(this)),
-      m_accountsWidget(new AccountsWidget),
-      m_accountsWorker(new AccountsWorker(m_userList, this))
+
+      m_accountsWidget(nullptr)
 {
-    connect(m_accountsWidget, &AccountsWidget::showAccountsDetail, this, &AccountsModule::showAccountsDetail);
 }
 
 void AccountsModule::initialize()
 {
-//    m_accountsInter->setSync(false);
-
-//    connect(m_accountsInter, &Accounts::UserListChanged, this, &AccountsModule::onUserListChanged);
-
-//    onUserListChanged(m_accountsInter->userList());
-
-    connect(m_userList, &UserModel::userAdded, m_accountsWidget, &AccountsWidget::addUser);
-    connect(m_userList, &UserModel::userRemoved, m_accountsWidget, &AccountsWidget::removeUser);
+    m_userList = new UserModel;
+    m_accountsWorker = new AccountsWorker(m_userList);
 }
 
 void AccountsModule::moduleActive()
 {
-//    m_accountsInter->blockSignals(false);
-//    m_accountsInter->getAllProperties();
 }
 
 void AccountsModule::moduleDeactive()
 {
-//    if (m_accountsWidget)
-//    {
-//        m_accountsWidget->deleteLater();
-//        m_accountsWidget = nullptr;
-//    }
-
-//    m_accountsInter->blockSignals(true);
 }
 
 ModuleWidget *AccountsModule::moduleWidget()
 {
+    if (!m_accountsWidget)
+    {
+        m_accountsWidget = new AccountsWidget;
+
+        connect(m_accountsWidget, &AccountsWidget::showAccountsDetail, this, &AccountsModule::showAccountsDetail);
+        connect(m_userList, &UserModel::userAdded, m_accountsWidget, &AccountsWidget::addUser);
+        connect(m_userList, &UserModel::userRemoved, m_accountsWidget, &AccountsWidget::removeUser);
+    }
+
     return m_accountsWidget;
 }
 
@@ -52,7 +44,8 @@ const QString AccountsModule::name() const
 
 AccountsModule::~AccountsModule()
 {
-
+    m_userList->deleteLater();
+    m_accountsWorker->deleteLater();
 }
 
 void AccountsModule::showAccountsDetail(User *account)
