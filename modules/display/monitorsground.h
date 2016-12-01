16e6358b21f9b35eaf5af9c25fbd44a766a0a7de
石博文 <sbw@sbw.so>
2016-12-01 17:24:01 +0800

    optimize screen position adjust
    Change-Id: I25bcb0936ac26dad50e63972f86a2fe74b2849a2

--- modules/display/monitorsground.h
+++ modules/display/monitorsground.h
@@ -22,11 +22,16 @@ private slots:
     void adjust(MonitorProxyWidget *pw);
 
 private:
+    void ensureWidgetPerfect(MonitorProxyWidget *pw);
+    int totalWidth() const;
+    int totalHeight() const;
     double screenScale() const;
 
 private:
+    int m_viewPortWidth;
+    int m_viewPortHeight;
     DisplayModel *m_model;
-    QMap<Monitor *, MonitorProxyWidget *> m_monitors;
+    QMap<MonitorProxyWidget *, Monitor *> m_monitors;
 };
 
 #endif // MONITORSGROUND_H
