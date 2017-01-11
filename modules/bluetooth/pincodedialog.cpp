d94955d6df0ca9d670342505a835d4124000873e
Hualet Wang <mr.asianwang@gmail.com>
2017-01-11 20:46:42 +0800

    bluetooth: Fix pincode text color not correct
    Change-Id: I18cd3eb18d054b4eee6bb36620caaf1c34ec7846

--- modules/bluetooth/pincodedialog.cpp
+++ modules/bluetooth/pincodedialog.cpp
@@ -24,9 +24,6 @@ PinCodeDialog::PinCodeDialog(const QString &pinCode, const bool &cancelable) :
     setIcon(QIcon::fromTheme("notification-bluetooth-connected"));
 
     m_pinCodeLabel->setObjectName("PinCodeText");
-    // FIXME(hualet): don't know why the style sheet cannot be moved to dark.qss,
-    // maybe DDialog's using style sheets concerns, fix it later.
-    m_pinCodeLabel->setStyleSheet("color: white");
     addContent(m_pinCodeLabel, Qt::AlignTop);
 
     QStringList btns;
