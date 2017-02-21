915a79dfd14734981b0dd072172d7e57754b912d
Hualet Wang <mr.asianwang@gmail.com>
2017-02-21 15:37:09 +0800

    Log to file
    Change-Id: Ieef27cc5dfe07016109f16f7d64f1aa59db3b2da

--- frame/main.cpp
+++ frame/main.cpp
@@ -124,6 +124,7 @@ int main(int argc, char *argv[])
     app.loadTranslator();
 
     DLogManager::registerConsoleAppender();
+    DLogManager::registerFileAppender();
 
     qApp->setQuitOnLastWindowClosed(false);
 
