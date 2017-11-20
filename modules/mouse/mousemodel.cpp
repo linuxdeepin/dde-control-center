94407af4237e7931fff89b08ff7f9e174cc70e97
kirigaya <kirigaya@mkacg.com>
2017-11-20 11:41:25 +0800

    refactor(mouse): integration model
    Change-Id: Ib66c3e9cdb094a63c85e4874225130c2f53f6d78

--- modules/mouse/mousemodel.cpp
+++ modules/mouse/mousemodel.cpp
@@ -63,3 +63,133 @@ MouseModelThinkpadSettings *MouseModel::getTrackSettings() const
 {
     return m_trackSettings;
 }
+
+void MouseModel::setLeftHandState(const bool state)
+{
+    if (m_leftHandState == state)
+        return;
+
+    m_leftHandState = state;
+
+    emit leftHandStateChanged(state);
+}
+
+void MouseModel::setDisIfTyping(const bool state)
+{
+    if (m_disIfTyping != state)
+        return;
+
+    m_disIfTyping = state;
+
+    emit disIfTypingStateChanged(state);
+}
+
+void MouseModel::setTpadExist(bool tpadExist)
+{
+    if (m_tpadExist == tpadExist)
+        return;
+
+    m_tpadExist = tpadExist;
+
+    emit tpadExistChanged(tpadExist);
+}
+
+void MouseModel::setMouseExist(bool mouseExist)
+{
+    if (m_mouseExist == mouseExist)
+        return;
+
+    m_mouseExist = mouseExist;
+
+    emit mouseExistChanged(mouseExist);
+}
+
+void MouseModel::setRedPointExist(bool redPointExist)
+{
+    if (m_redPointExist == redPointExist)
+        return;
+
+    m_redPointExist = redPointExist;
+
+    emit redPointExistChanged(redPointExist);
+}
+
+void MouseModel::setDoubleSpeed(int doubleSpeed)
+{
+    if (m_doubleSpeed == doubleSpeed)
+        return;
+
+    m_doubleSpeed = doubleSpeed;
+
+    emit doubleSpeedChanged(doubleSpeed);
+}
+
+void MouseModel::setMouseNaturalScroll(bool mouseNaturalScroll)
+{
+    if (m_mouseNaturalScroll == mouseNaturalScroll)
+        return;
+
+    m_mouseNaturalScroll = mouseNaturalScroll;
+
+    emit mouseNaturalScrollChanged(mouseNaturalScroll);
+}
+
+void MouseModel::setTpadNaturalScroll(bool tpadNaturalScroll)
+{
+    if (m_tpadNaturalScroll == tpadNaturalScroll)
+        return;
+
+    m_tpadNaturalScroll = tpadNaturalScroll;
+
+    emit tpadNaturalScrollChanged(tpadNaturalScroll);
+}
+
+void MouseModel::setMouseMoveSpeed(int mouseMoveSpeed)
+{
+    if (m_mouseMoveSpeed == mouseMoveSpeed)
+        return;
+
+    m_mouseMoveSpeed = mouseMoveSpeed;
+
+    emit mouseMoveSpeedChanged(mouseMoveSpeed);
+}
+
+void MouseModel::setTpadMoveSpeed(int tpadMoveSpeed)
+{
+    if (m_tpadMoveSpeed == tpadMoveSpeed)
+        return;
+
+    m_tpadMoveSpeed = tpadMoveSpeed;
+
+    emit tpadMoveSpeedChanged(tpadMoveSpeed);
+}
+
+void MouseModel::setDisTpad(bool disTpad)
+{
+    if (m_disTpad == disTpad)
+        return;
+
+    m_disTpad = disTpad;
+
+    emit disTpadChanged(disTpad);
+}
+
+void MouseModel::setRedPointMoveSpeed(int redPointMoveSpeed)
+{
+    if (m_redPointMoveSpeed == redPointMoveSpeed)
+        return;
+
+    m_redPointMoveSpeed = redPointMoveSpeed;
+
+    emit redPointMoveSpeedChanged(redPointMoveSpeed);
+}
+
+void MouseModel::setPalmDetect(bool palmDetect)
+{
+    if (m_palmDetect == palmDetect)
+        return;
+
+    m_palmDetect = palmDetect;
+
+    emit palmDetectChanged(palmDetect);
+}
--- modules/mouse/mousemodel.h
+++ modules/mouse/mousemodel.h
@@ -47,11 +47,78 @@ public:
     MouseModelMouseSettings *getTouchSettings() const;
     MouseModelThinkpadSettings *getTrackSettings() const;
 
+    inline bool leftHandState() const { return m_leftHandState; }
+    void setLeftHandState(const bool state);
+
+    void setDisIfTyping(const bool state);
+    inline bool getDisIfTyping()   const { return m_disIfTyping; }
+
+    inline bool tpadExist() const { return m_tpadExist; }
+    void setTpadExist(bool tpadExist);
+
+    inline bool mouseExist() const { return m_mouseExist; }
+    void setMouseExist(bool mouseExist);
+
+    inline bool redPointExist() const { return m_redPointExist; }
+    void setRedPointExist(bool redPointExist);
+
+    inline int doubleSpeed() const { return m_doubleSpeed; }
+    void setDoubleSpeed(int doubleSpeed);
+
+    inline bool mouseNaturalScroll() const { return m_mouseNaturalScroll; }
+    void setMouseNaturalScroll(bool mouseNaturalScroll);
+
+    inline bool tpadNaturalScroll() const { return m_tpadNaturalScroll; }
+    void setTpadNaturalScroll(bool tpadNaturalScroll);
+
+    inline int mouseMoveSpeed() const { return m_mouseMoveSpeed; }
+    void setMouseMoveSpeed(int mouseMoveSpeed);
+
+    inline int tpadMoveSpeed() const { return m_tpadMoveSpeed; }
+    void setTpadMoveSpeed(int tpadMoveSpeed);
+
+    inline bool disTpad() const { return m_disTpad; }
+    void setDisTpad(bool disTpad);
+
+    inline int redPointMoveSpeed() const { return m_redPointMoveSpeed; }
+    void setRedPointMoveSpeed(int redPointMoveSpeed);
+
+    inline bool palmDetect() const { return m_palmDetect; }
+    void setPalmDetect(bool palmDetect);
+
+signals:
+    void leftHandStateChanged(bool state);
+    void disIfTypingStateChanged(bool state);
+    void tpadExistChanged(bool tpadExist);
+    void mouseExistChanged(bool mouseExist);
+    void redPointExistChanged(bool rPointExist);
+    void doubleSpeedChanged(int speed);
+    void mouseNaturalScrollChanged(bool natural);
+    void tpadNaturalScrollChanged(bool natural);
+    void mouseMoveSpeedChanged(int speed);
+    void tpadMoveSpeedChanged(int speed);
+    void redPointMoveSpeedChanged(int speed);
+    void disTpadChanged(bool disable);
+    void palmDetectChanged(bool detect);
+
 private:
     MouseModelBaseSettings     *m_baseSettings;
     MouseModelMouseSettings    *m_mouseSettings;
     MouseModelMouseSettings    *m_touchSettings;
     MouseModelThinkpadSettings *m_trackSettings;
+    bool m_leftHandState;
+    bool m_disIfTyping;
+    bool m_tpadExist;
+    bool m_mouseExist;
+    bool m_redPointExist;
+    bool m_mouseNaturalScroll;
+    bool m_tpadNaturalScroll;
+    bool m_disTpad;
+    bool m_palmDetect;
+    int  m_doubleSpeed;
+    int  m_mouseMoveSpeed;
+    int  m_tpadMoveSpeed;
+    int  m_redPointMoveSpeed;
 };
 }
 }
