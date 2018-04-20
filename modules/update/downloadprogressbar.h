0922b5046440a2b7f38d7c6cb51cc132f32fd83b
haruyukilxz <kirigaya@mkacg.com>
2018-04-20 13:37:17 +0800

    refactor(update): Rewrite QProgressBar
    Change-Id: I9cbd680629b4d0656cf30c66ff3fe0ea4eb14ec3

--- modules/update/downloadprogressbar.h
+++ modules/update/downloadprogressbar.h
@@ -26,20 +26,14 @@
 #ifndef DOWNLOADPROGRESSBAR_H
 #define DOWNLOADPROGRESSBAR_H
 
-#include <QProgressBar>
+#include <QFrame>
 
 #include "common.h"
-
-namespace dcc {
-namespace widgets {
-class NormalLabel;
-}
-}
 
 namespace dcc{
 namespace update{
 
-class DownloadProgressBar : public QProgressBar
+class DownloadProgressBar : public QFrame
 {
     Q_OBJECT
 
@@ -47,15 +41,22 @@ public:
     explicit DownloadProgressBar(QWidget* parent = 0);
 
     void setMessage(const QString &message);
+    void setValue(const int progress);
+
+    inline const QString text() { return m_message; }
+    inline int minimum() { return 0; }
+    inline int maximum() { return 100; }
 
 protected:
     void mouseReleaseEvent(QMouseEvent *e);
+    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
 
 signals:
     void clicked();
 
 private:
-    dcc::widgets::NormalLabel *m_message;
+    int m_currentValue;
+    QString m_message;
 };
 
 }
