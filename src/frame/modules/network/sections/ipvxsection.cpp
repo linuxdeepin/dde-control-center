1b29385ef4f7c2c9a612c1dd86af23c143b9bdd8
listenerri <listenerri@gmail.com>
2019-03-19 10:46:57 +0800

    fix: ipv6 method change from manual to ignore can not save
    https://github.com/linuxdeepin/internal-discussion/issues/1121
    
    Change-Id: I2e746c6f7320de0deb92e2eb551e1c74eeced83b

--- src/frame/modules/network/sections/ipvxsection.cpp
+++ src/frame/modules/network/sections/ipvxsection.cpp
@@ -148,6 +148,7 @@ bool IpvxSection::saveIpv6Settings()
     ipv6Setting->setMethod(Ipv6ConfigMethodStrMap.value(m_methodChooser->value()));
 
     if (method == NetworkManager::Ipv6Setting::Ignored) {
+        ipv6Setting->setAddresses(QList<NetworkManager::IpAddress>());
         return true;
     }
 
