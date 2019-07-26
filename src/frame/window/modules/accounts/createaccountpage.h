6118be057d0245e2aad54e50c4e03779fe6d1269
liuhong <liuhong_cm@deepin.com>
2019-07-26 10:09:29 +0800

    feat: show account list infomation

---
+++ src/frame/window/modules/accounts/createaccountpage.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2011 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     liuhong <liuhong_cm@deepin.com>
+ *
+ * Maintainer: justforlxz <justforlxz@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CREATEACCOUNTPAGE_H
+#define CREATEACCOUNTPAGE_H
+
+#include <QWidget>
+
+class CreateAccountPage : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit CreateAccountPage(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // CREATEACCOUNTPAGE_H
