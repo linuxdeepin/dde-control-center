cada095fa1ff9562d14796f5936ca17ebd3f5496
justforlxz <justforlxz@outlook.com>
2019-08-05 14:13:13 +0800

    feat(sync): add login tip for login page

--- src/frame/window/modules/sync/pages/login.cpp
+++ src/frame/window/modules/sync/pages/login.cpp
@@ -15,9 +15,16 @@ LoginPage::LoginPage(QWidget *parent)
     m_mainLayout->setSpacing(0);
 
     QPushButton* loginBtn = new QPushButton(tr("Sign In"));
+    QLabel* tip = new QLabel;
+    tip->setText(tr("Sign in with Deepin ID and turn on Cloud Sync to sync the specific system settings."));
+    tip->setWordWrap(true);
+    tip->setAlignment(Qt::AlignCenter);
 
     m_mainLayout->addStretch();
-    m_mainLayout->addWidget(new QLabel("Cloud Sync"), 0, Qt::AlignHCenter);
+    m_mainLayout->addWidget(new QLabel(tr("Deepin ID Sign In")), 0, Qt::AlignHCenter);
+    m_mainLayout->addSpacing(10);
+    m_mainLayout->addWidget(tip);
+    m_mainLayout->addSpacing(10);
     m_mainLayout->addWidget(loginBtn, 0, Qt::AlignHCenter);
     m_mainLayout->addStretch();
 
