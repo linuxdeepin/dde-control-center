94d2e28ec87b281f89ef5c1f52feaf7dc9305893
liuhong <liuhong_cm@deepin.com>
2019-09-29 10:29:29 +0800

    fix(account): Mouse enters the bottom of the image to display a gradient

--- src/frame/modules/accounts/avatarwidget.cpp
+++ src/frame/modules/accounts/avatarwidget.cpp
@@ -31,7 +31,8 @@
 #include <QPaintEvent>
 #include <QVBoxLayout>
 #include <QApplication>
-
+#include <QRect>
+#include <QColor>
 
 using namespace dcc::accounts;
 
@@ -144,6 +145,27 @@ void AvatarWidget::paintEvent(QPaintEvent *e)
         painter.drawEllipse(rect());
     };
 
+    //当鼠标移动到图像上面
+    if (m_hover) {
+        painter.setPen(Qt::NoPen);
+        //宽高
+        int w = this->rect().width();
+        int h = this->rect().height();
+        //矩形
+        QRect rect(0, h - h / 4, w, h - h / 4);
+        // 反走样
+        painter.setRenderHint(QPainter::Antialiasing, true);
+        // 设置渐变色
+        QLinearGradient linear(QPoint(0, h - h / 4), QPoint(0, h));
+        linear.setColorAt(0, QColor(0, 0, 0, 0.00 * 255));
+        linear.setColorAt(1, QColor(0, 0, 0, 0.50 * 255));
+
+        // 设置显示模式
+        linear.setSpread(QGradient::PadSpread);
+        painter.setBrush(linear);
+        painter.drawRect(rect);
+    }
+
     if (m_arrowed) {
         QPen pen(Qt::transparent);
         pen.setWidth(2);
@@ -159,6 +181,21 @@ void AvatarWidget::paintEvent(QPaintEvent *e)
         //绘制右边直线
         painter.drawLine(QPoint(cpt.x() + portion / 2, cpt.y() + portion * 4 - portion / 2),
                          QPoint(cpt.x(), cpt.y() + portion * 4));
+    } else {
+        QPen pen(Qt::transparent);
+        pen.setWidth(2);
+        pen.setColor(Qt::white);
+        painter.setPen(pen);
+        //把直径平均分成10份
+        int portion = this->rect().width() / 10;
+        //圆中心点坐标
+        QPoint cpt = this->rect().center();
+        //绘制左边直线
+        painter.drawLine(QPoint(cpt.x() - portion / 2, cpt.y() + portion * 4),
+                         QPoint(cpt.x(), cpt.y() + portion * 4 - portion / 2));
+        //绘制右边直线
+        painter.drawLine(QPoint(cpt.x() + portion / 2, cpt.y() + portion * 4),
+                         QPoint(cpt.x(), cpt.y() + portion * 4 - portion / 2));
     }
 
     QWidget::paintEvent(e);
