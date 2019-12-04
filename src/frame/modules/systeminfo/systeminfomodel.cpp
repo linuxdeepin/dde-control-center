673edf4acf4af53da1191e7bed22eb9e898cac76
longqi_cm@deepin.com <longqi_cm@deepin.com>
2019-12-04 20:12:33 +0800

    fix(systeminfo): round memory total size

--- src/frame/modules/systeminfo/systeminfomodel.cpp
+++ src/frame/modules/systeminfo/systeminfomodel.cpp
@@ -41,7 +41,7 @@ static QString formatCap(qulonglong cap, const int size = 1024, quint8 precision
     for(size_t p = 0; p < sizeof(type); ++p) {
         if (cap < pow(size, p + 1) || p == sizeof(type) - 1) {
             if (!precision) {
-                return QString::number(lc / qulonglong(pow(size, p))) + type[p];
+                return QString::number(round(lc / pow(size, p))) + type[p];
             }
 
             return QString::number(dc / pow(ds, p), 'f', precision) + type[p];
