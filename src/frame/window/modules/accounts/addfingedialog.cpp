4e420a7826e08a6022623137625eebd599b5193f
yangyuyin_cm <yangyuyin_cm@deepin.com>
2020-02-20 22:38:09 +0800

    feat: 测试并修改代码

--- src/frame/window/modules/accounts/addfingedialog.cpp
+++ src/frame/window/modules/accounts/addfingedialog.cpp
@@ -100,12 +100,22 @@ void AddFingeDialog::initData()
     m_addBtn->setText((tr("添加")));
     connect(m_cancelBtn, &QPushButton::clicked, this, &AddFingeDialog::close);
     connect(m_scanBtn, &QPushButton::clicked, this, &AddFingeDialog::requestReEnrollThumb);//重新录入后，界面变化还需要再确认
+    connect(m_scanBtn, &DSuggestButton::clicked, this, &AddFingeDialog::setInitStatus);//录入中断，或断开超时
     connect(m_addBtn, &QPushButton::clicked, this, &AddFingeDialog::requestEnrollThumb);
+    connect(m_scanBtn, &QPushButton::clicked, this, &AddFingeDialog::close);
+    connect(m_doneBtn, &QPushButton::clicked, this, &AddFingeDialog::close);
 }
 
 void AddFingeDialog::setFingerModel(FingerModel *model)
 {
     m_model = model;
+    connect(m_model, &FingerModel::enrollCompleted, this, &AddFingeDialog::enrollCompleted);
+    connect(m_model, &FingerModel::enrollStagePass, this, &AddFingeDialog::enrollStagePass);
+    connect(m_model, &FingerModel::enrollFailed, this, &AddFingeDialog::enrollFailed);
+    connect(m_model, &FingerModel::enrollDisconnected, this, &AddFingeDialog::enrollDisconnected);
+    connect(m_model, &FingerModel::enrollRetry, this, &AddFingeDialog::enrollRetry);
+
+//    connect(m_model, &FingerModel::enrollFailed, this, &AddFingeDialog::enrollFailed)
 }
 
 void AddFingeDialog::setUsername(const QString &name)
@@ -116,6 +126,72 @@ void AddFingeDialog::setUsername(const QString &name)
 void AddFingeDialog::setDefaultMsgTip()
 {
     m_fingeWidget->setFrequency(tr("请以手指按压指纹收集器，然后根据提示抬起"));
+}
+
+void AddFingeDialog::enrollCompleted()
+{
+    m_titleTip->setText(tr("指纹录入完成！"));
+    m_fingeWidget->setFrequency("");
+    m_fingeWidget->finished();
+    m_doneBtn->setVisible(true);
+    m_addBtn->setVisible(false);
+}
+
+void AddFingeDialog::enrollStagePass(int pro)
+{
+    if (pro < 35) {
+        m_titleTip->setVisible(false);
+        m_fingeWidget->setFrequency(tr("请抬起手指，再次按压"));
+        m_fingeWidget->next();
+    }
+    if (pro >= 35 && pro <100) {
+        m_fingeWidget->setFrequency(tr("请抬起手指，调整按压区域，继续录入边缘指纹"));
+    }
+}
+
+void AddFingeDialog::enrollFailed(QString msg)
+{
+    m_titleTip->setText("录入中断");
+    m_fingeWidget->setFrequency(msg);
+    m_addBtn->setVisible(false);
+    m_scanBtn->setVisible(true);
+}
+
+void AddFingeDialog::setInitStatus()
+{
+    m_cancelBtn->setVisible(true);
+    m_addBtn->setVisible(true);
+    m_scanBtn->setVisible(false);
+    m_doneBtn->setVisible(false);
+    m_titleTip->setText(tr("放置手指"));
+    m_fingeWidget->setFrequency(tr("请以手指压指纹收集器，然后根据提示抬起"));
+}
+
+void AddFingeDialog::enrollDisconnected()
+{
+    m_titleTip->setText(tr("录入中断"));
+    m_fingeWidget->setFrequency(tr("录入中断"));
+    m_addBtn->setVisible(false);
+    m_scanBtn->setVisible(true);
+}
+
+void AddFingeDialog::enrollRetry(QString msg)
+{
+    m_qtimerTitleTip->stop();
+    m_qtimerTitleTip->setInterval(1000);
+    m_titleTip->setText(tr("无法识别"));
+    connect(m_qtimerTitleTip, &QTimer::timeout, this, [this] {
+        setDefaultTitleTip();
+    });
+    m_qtimerTitleTip->start();
+
+    m_qtimerMsgTip->setSingleShot(true);
+    m_fingeWidget->setFrequency(msg);
+    m_qtimerMsgTip->setInterval(2000);
+    connect(m_qtimerMsgTip, &QTimer::timeout, [this] {setDefaultMsgTip();});
+    m_qtimerMsgTip->start();
+
+
 }
 
 void AddFingeDialog::onViewPlayEnd()
