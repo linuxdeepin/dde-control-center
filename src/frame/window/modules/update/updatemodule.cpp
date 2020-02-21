7cdf9d198e96d16b6d604902d5cd44e38a0b5564
王雷勇 <wanleiyong_cm@deepin.com>
2020-02-21 10:49:59 +0800

    fix(update) 终端登录本地管理员账户，控制中心——应该被裁剪的更新栏还存在
    Fix(13571): 根据配置文件显示或隐藏控制中心的更新栏

--- src/frame/window/modules/update/updatemodule.cpp
+++ src/frame/window/modules/update/updatemodule.cpp
@@ -25,6 +25,7 @@
 #include "mirrorswidget.h"
 #include "modules/systeminfo/systeminfomodel.h"
 #include "modules/systeminfo/systeminfowork.h"
+#include "widgets/utils.h"
 
 #include <QVBoxLayout>
 
@@ -76,6 +77,10 @@ void UpdateModule::preInitialize(bool sync)
     });
     connect(m_model, &UpdateModel::statusChanged, this, &UpdateModule::notifyDisplayReminder);
     notifyDisplayReminder(m_model->status());
+
+    auto req = QDBusConnection::sessionBus().interface()->isServiceRegistered("com.deepin.deepinid");
+    bool bShowUpdate = req.value() && valueByQSettings<bool>(DCC_CONFIG_FILES, "", "showUpdate", false);
+    m_frameProxy->setModuleVisible(this, bShowUpdate);
 }
 
 void UpdateModule::initialize()
