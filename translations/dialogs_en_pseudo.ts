4845574c51852ce1fb88de091efdd8734e385438
songwentai <songwentai@deepin.com>
2020-02-23 02:27:46 +0000

    chore: auto pull translation files from transifex

---
+++ translations/dialogs_en_pseudo.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
+<context>
+    <name>ReminderDialog</name>
+    <message>
+        <location filename="../src/dialogs/reminderdialog.cpp" line="35"/>
+        <source>Restart the computer to use the system and the applications properly</source>
+        <translation>[Řḗşŧȧřŧ ŧħḗ ƈǿḿƥŭŧḗř ŧǿ ŭşḗ ŧħḗ şẏşŧḗḿ ȧƞḓ ŧħḗ ȧƥƥŀīƈȧŧīǿƞş ƥřǿƥḗřŀẏ ϵſǈϕǋǅϑ ϰϐ]</translation>
+    </message>
+    <message>
+        <location filename="../src/dialogs/reminderdialog.cpp" line="36"/>
+        <source>Make sure all data had been saved first</source>
+        <translation>[Ḿȧķḗ şŭřḗ ȧŀŀ ḓȧŧȧ ħȧḓ ƀḗḗƞ şȧṽḗḓ ƒīřşŧ ẛǋϱς ϐǋΐ靐]</translation>
+    </message>
+    <message>
+        <location filename="../src/dialogs/reminderdialog.cpp" line="45"/>
+        <source>Cancel</source>
+        <translation>[Ƈȧƞƈḗŀ ıςΐ衋ϐϰ]</translation>
+    </message>
+    <message>
+        <location filename="../src/dialogs/reminderdialog.cpp" line="45"/>
+        <source>Shut down</source>
+        <translation>[Şħŭŧ ḓǿẇƞ ϑϕϵǋ ϐϕǈ鶱]</translation>
+    </message>
+    <message>
+        <location filename="../src/dialogs/reminderdialog.cpp" line="48"/>
+        <source>Reboot</source>
+        <translation>[Řḗƀǿǿŧ 衋ſǅϖϕϵ]</translation>
+    </message>
+</context>
+</TS>
