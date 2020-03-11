605a932f181381375f254f40aa3eeb51905d306a
longqi <longqi@uniontech.com>
2020-03-11 23:02:06 +0800

    fix(dipslay): customsettingdialog add close btn

--- src/frame/window/modules/display/customsettingdialog.cpp
+++ src/frame/window/modules/display/customsettingdialog.cpp
@@ -200,9 +200,11 @@ void CustomSettingDialog::initOtherDialog()
         } else {
             dlg = new CustomSettingDialog(mon, m_model, this);
             m_otherDialog.append(dlg);
+            dlg->setWindowFlags(dlg->windowFlags() | Qt::WindowCloseButtonHint);
 
             dlg->initConnect();
 
+            connect(dlg, &DDialog::closed, this, &CustomSettingDialog::close);
             connect(dlg, &CustomSettingDialog::requestSetResolution, this,
                     &CustomSettingDialog::requestSetResolution);
             connect(dlg, &CustomSettingDialog::requestShowRotateDialog, this,
