ac43d3fcd93ad7d6e6c141969cd61ca94791bdb6
quezhiyong <quezhiyong@uniontech.com>
2020-03-26 15:02:46 +0800

    fix(accounts): prohibit deleting current account
    pms.bug: 17885
    
    Change-Id: I5b1903fd7c212404f2fcdb5eefe1dc16a37a07e5
    Reviewed-on: http://gerrit.uniontech.com/c/dde-control-center/+/1817
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: huangjiawei <huangjiawei@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/frame/window/modules/accounts/accountsdetailwidget.cpp
+++ src/frame/window/modules/accounts/accountsdetailwidget.cpp
@@ -394,8 +394,7 @@ void AccountsDetailWidget::initSetting(QVBoxLayout *layout)
     m_nopasswdLogin->setChecked(m_curUser->nopasswdLogin());
 
     //当前用户禁止使用删除按钮
-    const bool isOnline = m_curUser->online();
-    deleteAccount->setEnabled(!isOnline);
+    deleteAccount->setEnabled(!m_curUser->isCurrentUser());
 
     connect(m_curUser, &User::onlineChanged, this, [=] (const bool online) {
         deleteAccount->setDisabled(online);
