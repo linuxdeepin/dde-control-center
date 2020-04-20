734dcf473dcb76a2958f24f189776d534ad73301
litao <litao_cm@deepin.com>
2020-04-20 17:45:38 +0800

    fix(update):修改了更新包只显示系统补丁一个包的问题.
    Fix(21601) 系统有应用更新，在控制中心更新后，系统更新和应用更新没有分开显示，只显示系统补丁

--- src/frame/modules/update/updatework.cpp
+++ src/frame/modules/update/updatework.cpp
@@ -355,10 +355,10 @@ void UpdateWorker::setAppUpdateInfo(const AppUpdateInfoList &list)
             ddeUpdateInfo.m_changelog = tr("System patches");
         }
 
-        if(!DCC_NAMESPACE::IsDesktopSystem) {
-            //app updates are not displayed
-            infos.clear();
-        }
+//        if(!DCC_NAMESPACE::IsDesktopSystem) {
+//            //app updates are not displayed
+//            infos.clear();
+//        }
 
         infos.prepend(ddeUpdateInfo);
     }
