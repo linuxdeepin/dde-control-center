3692ccf928f8fa0cf2de0b4775415607ccfe067e
guoyao <guoyao@deepin.com>
2020-04-21 05:06:09 +0000

    feat:新增翻译脚本
    (cherry picked from commit 2d9c6bf7952ede8544e014b193b1734ec590c812)
    
    (cherry picked from commit cdff0dacfdae02b83e9c6a3dc5c8eb364a409c76)
    
    (cherry picked from commit d5fcc9e796a4a66b3cb5359386dfba99d576f110)

---
+++ lupdate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp ".transifexrc" ${HOME}/
+cd ./translations
+rm -f dde-control-center_en_US.ts
+lupdate ../src/ -ts dde-control-center_en_US.ts
+sed -e 's/DCC_NAMESPACE/dccV20/g' dde-control-center_en_US.ts > tmp.ts
+mv tmp.ts dde-control-center_en_US.ts
+cd ../
+tx push -s
