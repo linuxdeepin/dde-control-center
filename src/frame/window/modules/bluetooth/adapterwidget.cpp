29ed607205f236bcc27b02ecc385725ad36b6792
chengbo_cm <chengbo_cm@deepin.com>
2020-04-27 14:42:42 +0800

    "fix(bluetooth): 修复无法正常关闭蓝牙或关闭延迟"

--- src/frame/window/modules/bluetooth/adapterwidget.cpp
+++ src/frame/window/modules/bluetooth/adapterwidget.cpp
@@ -201,18 +201,23 @@ void AdapterWidget::setAdapter(const Adapter *adapter)
 
 void AdapterWidget::onPowerStatus(bool bPower)
 {
-    m_switch->setChecked(bPower);
-    m_tip->setVisible(!bPower);
-    m_myDevicesGroup->setVisible(bPower && !m_myDevices.isEmpty());
-    m_otherDevicesGroup->setVisible(bPower);
-    m_spinner->setVisible(bPower);
-    m_myDeviceListView->setVisible(bPower && !m_myDevices.isEmpty());
-    m_otherDeviceListView->setVisible(bPower);
-    Q_EMIT notifyLoadFinished();
+    if (getSwitchState() == bPower) {
+        m_switch->setChecked(bPower);
+        m_tip->setVisible(!bPower);
+        m_myDevicesGroup->setVisible(bPower && !m_myDevices.isEmpty());
+        m_otherDevicesGroup->setVisible(bPower);
+        m_spinner->setVisible(bPower);
+        m_myDeviceListView->setVisible(bPower && !m_myDevices.isEmpty());
+        m_otherDeviceListView->setVisible(bPower);
+        Q_EMIT notifyLoadFinished();
+    } else {
+        Q_EMIT requestSetToggleAdapter(m_adapter, getSwitchState());
+    }
 }
 
 void AdapterWidget::toggleSwitch(const bool checked)
 {
+    onPowerStatus(checked);
     if (!checked) {
         for (auto it : m_myDevices) {
             if (it->device()->connecting()) {
