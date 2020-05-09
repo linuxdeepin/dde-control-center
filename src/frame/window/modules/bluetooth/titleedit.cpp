1004db8e7bf879c2c0fb80c0c1c4e1ed4a206abd
dongrui <1160899437@qq.com>
2020-05-09 15:30:36 +0800

    fix(bluetooth):no alert when bluetooeh name over 32 byte
    FIX(23480):alert voice

--- src/frame/window/modules/bluetooth/titleedit.cpp
+++ src/frame/window/modules/bluetooth/titleedit.cpp
@@ -26,7 +26,7 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QHBoxLayout>
-#include <DLineEdit>
+#include <DDesktopServices>
 
 DWIDGET_USE_NAMESPACE
 
@@ -56,9 +56,7 @@ TitleEdit::TitleEdit(QWidget *parent)
     connect(m_lineEdit, &DLineEdit::textEdited, this, [ = ](const QString &str){
         if (str.length() > 32) {
             m_lineEdit->lineEdit()->backspace();
-            m_lineEdit->setAlert(true);
-        } else {
-            m_lineEdit->setAlert(false);
+            DDesktopServices::playSystemSoundEffect(DDesktopServices::SSE_Error);
         }
     });
     connect(editWidget, &DIconButton::clicked, this, &TitleEdit::setEdit);
