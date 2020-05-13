27d06eef0c33551aa3bc49c5944136a4ff074e49
hjw <huangjiawei@uniontech.com>
2020-05-13 17:23:54 +0800

    fix:When setting the resolution, the prompt box appears outside the screen with high probability
    -修复切换分辨率时高概率提示框出现在屏幕之外bug，紧急bug暂时无单号后续补

--- src/frame/main.cpp
+++ src/frame/main.cpp
@@ -52,6 +52,7 @@ void closeSignal(int s) {
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_WAYLAND_SHELL_INTEGRATION", "kwayland-shell");
     DApplication app(argc, argv);
     app.setOrganizationName("deepin");
     app.setApplicationName("dde-control-center");
