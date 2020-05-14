8d03de243b9120ad098d744f3007656ce49034e2
zhangqipeng <zhangqipeng@uniontech.com>
2020-05-14 11:17:55 +0800

    Feat(translation): Modify translation script file
    修改翻译脚本文件中分支名。

--- lupdate.sh
+++ lupdate.sh
@@ -6,4 +6,4 @@ lupdate ../src/ -ts dde-control-center_en_US.ts
 sed -e 's/DCC_NAMESPACE/dccV20/g' dde-control-center_en_US.ts > tmp.ts
 mv tmp.ts dde-control-center_en_US.ts
 cd ../
-tx push -s -b pg
+tx push -s -b klu
