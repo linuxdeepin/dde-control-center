78c2d00dd4d199090ee0a9e733c0ea79252372e0
黄伟华 <huangweihua_cm@deepin.com>
2020-05-18 17:40:11 +0800

    Revert "Revert "Revert "feat(network): Restore the clear-text Password for the user connection...""
    This reverts commit 5bda76b86a6c87b28dc89e268e06b323e79a0281

--- src/frame/window/modules/network/connectioneditpage.cpp
+++ src/frame/window/modules/network/connectioneditpage.cpp
@@ -401,6 +401,7 @@ void ConnectionEditPage::createConnSettings()
     }
     m_connectionUuid = m_connectionSettings->createNewUuid();
     m_connectionSettings->setUuid(m_connectionUuid);
+    m_connectionSettings->setting(Setting::Security8021x).staticCast<NetworkManager::Security8021xSetting>()->setPasswordFlags(Setting::AgentOwned);
 }
 
 int ConnectionEditPage::connectionSuffixNum(const QString &matchConnName)
