1ba992ba9b8afb79955a8f6ec6924c95496fd27a
dongrui <1160899437@qq.com>
2020-06-03 15:38:35 +0800

    feat(power):修改文案
    修改文案
    
    log:文案修改
    
    task: https://pms.uniontech.com/zentao/task-view-22351.html
    Change-Id: Iee8ad82e8a5fa036b0bf997690970f5d8db651cb

--- src/frame/window/modules/power/generalwidget.cpp
+++ src/frame/window/modules/power/generalwidget.cpp
@@ -40,7 +40,7 @@ GeneralWidget::GeneralWidget(QWidget *parent, bool bIsBattery)
     : QWidget(parent)
     , m_layout(new QVBoxLayout)
     , m_lowBatteryMode(new SwitchWidget(tr("Power Saving Mode")))
-    , m_autoIntoSaveEnergyMode(new SwitchWidget(tr("Auto Mode Switch")))
+    , m_autoIntoSaveEnergyMode(new SwitchWidget(tr("Auto power saving on battery")))
     , m_wakeComputerNeedPassword(new SwitchWidget(tr("Password is required to wake up the computer")))
     , m_wakeDisplayNeedPassword(new SwitchWidget(tr("Password is required to wake up the monitor")))
 {
@@ -52,7 +52,7 @@ GeneralWidget::GeneralWidget(QWidget *parent, bool bIsBattery)
     //~ contents_path /power/General
     m_lowBatteryMode->setTitle(tr("Power Saving Mode"));
     //~ contents_path /power/General
-    m_autoIntoSaveEnergyMode->setTitle(tr("Auto Mode Switch"));
+    m_autoIntoSaveEnergyMode->setTitle(tr("Auto power saving on battery"));
     //~ contents_path /power/General
     m_wakeComputerNeedPassword->setTitle(tr("Password is required to wake up the computer"));
     //~ contents_path /power/General
