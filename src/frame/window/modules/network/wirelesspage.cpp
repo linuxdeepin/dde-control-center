222e5c29747a9c96bf739fc23f628a360d9b3f5d
Lvpeilong <lvpeilong@deepin.com>
2020-06-10 21:28:48 +0800

    feat(network): 【控制中心】可能崩溃
    修复在无线网络信号频繁在信号弱（5以下）与有信号时，对该无线网络进行配置时网络信号弱，从列表中删除后保存配置可能导致系统退出的问题
    
    log:更改信号弱的AP不显示机制， 控制无线AP隐藏条件，已连接和正连接的AP不隐藏
    
    task: https://pms.uniontech.com/zentao/task-view-26603.html
    Change-Id: I84dd8bfcbcb7fe88b6b94c746ad63aa46414adee

--- src/frame/window/modules/network/wirelesspage.cpp
+++ src/frame/window/modules/network/wirelesspage.cpp
@@ -491,6 +491,19 @@ void WirelessPage::onAPChanged(const QJsonObject &apInfo)
     const bool isSecure = apInfo.value("Secured").toBool();
 
     APItem *it = m_apItems[ssid];
+
+    if (5 >= strength && !it->checkState() && ssid != m_device->activeApSsid()) {
+        if (nullptr == m_clickedItem) {
+            m_lvAP->setRowHidden(it->row(), true);
+        } else if (it->uuid() != m_clickedItem->uuid()) {
+            m_lvAP->setRowHidden(it->row(), true);
+        }
+
+
+    } else {
+        m_lvAP->setRowHidden(it->row(), false);
+    }
+
     APSortInfo si{strength, ssid, ssid == m_device->activeApSsid()};
     m_apItems[ssid]->setSortInfo(si);
 
@@ -689,13 +702,10 @@ void WirelessPage::updateActiveAp()
                 });
             }
         } else {
-            bool isReconnect = it.value()->setLoading(false);
-            if (isReconnect) {
-                connect(it.value()->action(), &QAction::triggered, this, [this, it] {
-                    this->onApWidgetEditRequested(it.value()->data(APItem::PathRole).toString(),
-                                                  it.value()->data(Qt::ItemDataRole::DisplayRole).toString());
-                });
-            }
+            connect(it.value()->action(), &QAction::triggered, this, [this, it] {
+                this->onApWidgetEditRequested(it.value()->data(APItem::PathRole).toString(),
+                                              it.value()->data(Qt::ItemDataRole::DisplayRole).toString());
+            });
         }
     }
     if (isWifiConnected && m_clickedItem) {
