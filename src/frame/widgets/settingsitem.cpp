f46319306136d97a3319a34aad7847ceb1df30dc
litao <litao_cm@deepin.com>
2020-06-12 15:00:39 +0800

    fix(settingsitem): UI interface problem - background list background fillet length log: UI界面问题-背景列表背景圆角长短 bug: https://pms.uniontech.com/zentao/bug-view-30399.html

--- src/frame/widgets/settingsitem.cpp
+++ src/frame/widgets/settingsitem.cpp
@@ -64,7 +64,7 @@ void SettingsItem::addBackground()
     if (m_bgGroup)
         m_bgGroup->deleteLater();
     m_bgGroup = new DFrame(this);
-    m_bgGroup->setContentsMargins(0, 0, 0, 0);
+//    m_bgGroup->setContentsMargins(0, 0, 0, 0);
     m_bgGroup->setBackgroundRole(DPalette::ItemBackground);
     m_bgGroup->setLineWidth(0);
     DStyle::setFrameRadius(m_bgGroup, 8);
