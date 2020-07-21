463baa9b7e3e517b4b3310942e6fa5753e5ce5e6
dongrui <dongrui@uniontech.com>
2020-07-21 17:35:54 +0800

    fix(update): 屏幕缩放调到1.25倍后查看更新,更新图标锯齿明显
    为更新图标添加抗锯齿
    
    Log: 屏幕缩放调到1.25倍后查看更新,更新图标锯齿明显
    Bug: https://pms.uniontech.com/zentao/bug-view-37187.html
    Change-Id: Ia73bf1be6748ce68df2b372ac225731cc67e6f6a

--- src/frame/window/modules/update/loadingitem.cpp
+++ src/frame/window/modules/update/loadingitem.cpp
@@ -26,6 +26,7 @@
 #include <QDebug>
 #include <QVBoxLayout>
 #include <QIcon>
+#include <QPainter>
 #include <DSysInfo>
 
 DCORE_USE_NAMESPACE
@@ -112,6 +113,9 @@ QPixmap LoadingItem::getPixmap(const QString &name, const QSize size)
     const QIcon &icon = QIcon(name);
     const qreal ratio = devicePixelRatioF();
     QPixmap pixmap = icon.pixmap(size * ratio).scaled(size * ratio, Qt::KeepAspectRatio, Qt::FastTransformation);
+    QPainter p(&pixmap);
+    p.setRenderHints(QPainter::Antialiasing);
+    p.drawPixmap(0, 0, pixmap);
     pixmap.setDevicePixelRatio(ratio);
     return pixmap;
 }
