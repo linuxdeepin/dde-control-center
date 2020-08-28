ef4127e6f2c968099986621680ec159f8924e54d
huangweihua <huangweihua@uniontech.com>
2020-08-28 09:51:20 +0800

    fix: 取消控件强聚焦
    继承于QAbstractButton/QLineEdit/QAbstractItemView/QAbstractSlider才会有焦点，获取焦点的控制使用setTabOrder进行设置
    
    Log: 更新/更新设置的图标点击之后的状态显示不对
    Bug: https://pms.uniontech.com/zentao/bug-view-22407.html
    Change-Id: Ia253329c77e0d929e56c3c8d78fd5a0e18c51772
    Reviewed-on: http://gerrit.uniontech.com/c/dde-control-center/+/2846
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: lizhongming <lizhongming@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/frame/window/mainwindow.cpp
+++ src/frame/window/mainwindow.cpp
@@ -264,7 +264,6 @@ void MainWindow::findFocusChild(QWidget *w, QWidget *&pre)
 void MainWindow::findFocusChild(QLayout *l, QWidget *&pre)
 {
     for (int i = 0; i < l->count(); ++i) {
-//        qDebug() << l->itemAt(i);
         auto cw = l->itemAt(i)->widget();
         auto cl = l->itemAt(i)->layout();
 
@@ -283,7 +282,6 @@ void MainWindow::findFocusChild(QLayout *l, QWidget *&pre)
                     && !qobject_cast<QLineEdit *>(cw)
                     && !qobject_cast<QAbstractItemView *>(cw)
                     && !qobject_cast<QAbstractSlider *>(cw)) || !cw->isEnabled()) {
-//                cw->setFocusPolicy(Qt::NoFocus);
                 continue;
             }
 
@@ -292,8 +290,6 @@ void MainWindow::findFocusChild(QLayout *l, QWidget *&pre)
                 continue;
             }
 
-            if (!qobject_cast<QAbstractSlider *>(cw))
-                cw->setFocusPolicy(Qt::StrongFocus);
             setTabOrder(pre, cw);
             pre = cw;
         } else if (cl) {
