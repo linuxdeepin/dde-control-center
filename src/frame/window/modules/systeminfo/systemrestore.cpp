e728a214cd2929c406214b079101f1393c1d5c68
zhaolong <zhaolong@uniontech.com>
2020-09-21 13:07:14 +0800

    fix(systeminfo): 路径选择按扭无法点击
    可能是qt5.15升级致使QStackLayout出问题，现用QTabwidget对此问题暂作规避
    
    Log: 修复系统备份还原功能路径选择按钮无法点击问题
    Task: https://pms.uniontech.com/zentao/task-view-34725.html
    Change-Id: I90653c704de531a76209a7aff4d1f15db95883cf
    Reviewed-on: http://gerrit.uniontech.com/c/dde-control-center/+/5651
    Reviewed-by: xiehui <xiehui@uniontech.com>
    Reviewed-by: zhaolong <zhaolong@uniontech.com>
    Tested-by: hudeng <hudeng@uniontech.com>

--- src/frame/window/modules/systeminfo/systemrestore.cpp
+++ src/frame/window/modules/systeminfo/systemrestore.cpp
@@ -7,6 +7,7 @@
 #include <QPushButton>
 #include <QStackedLayout>
 #include <QTimer>
+#include <QEvent>
 
 using namespace DCC_NAMESPACE;
 using namespace DCC_NAMESPACE::systeminfo;
@@ -15,7 +16,7 @@ DWIDGET_USE_NAMESPACE
 
 SystemRestore::SystemRestore(BackupAndRestoreModel* model, QWidget *parent)
     : QWidget(parent)
-    , m_buttonBox(new DButtonBox(this))
+//    , m_buttonBox(new DButtonBox(this)) 注释代码及新增改动为临时方案，解决QStacklayout问题后revert
     , m_backupPage(new ManualBackup(model))
     , m_restorePage(new ManualRestore(model))
 {
@@ -26,38 +27,49 @@ SystemRestore::SystemRestore(BackupAndRestoreModel* model, QWidget *parent)
     m_backupPage->setFileDialog(fileDialog);
     m_restorePage->setFileDialog(fileDialog);
 
-    DButtonBoxButton* backupBtn = new DButtonBoxButton(tr("Backup"));
-    DButtonBoxButton* restoreBtn = new DButtonBoxButton(tr("Restore"));
+//    DButtonBoxButton* backupBtn = new DButtonBoxButton(tr("Backup"));
+//    DButtonBoxButton* restoreBtn = new DButtonBoxButton(tr("Restore"));
 
-    m_buttonBox->setButtonList({backupBtn, restoreBtn}, true);
+//    m_buttonBox->setButtonList({backupBtn, restoreBtn}, true);
 
     QVBoxLayout *mainLayout = new QVBoxLayout;
     mainLayout->setMargin(0);
     mainLayout->setSpacing(0);
 
-    mainLayout->addWidget(m_buttonBox);
-    mainLayout->addSpacing(5);
 
-    m_stackedLayout = new QStackedLayout;
-    m_stackedLayout->addWidget(m_backupPage);
-    m_stackedLayout->addWidget(m_restorePage);
 
-    mainLayout->addLayout(m_stackedLayout);
+
+
+//    mainLayout->addWidget(m_buttonBox);
+//    mainLayout->addSpacing(5);
+
+//    m_stackedLayout = new QStackedLayout;
+//    m_stackedLayout->addWidget(m_backupPage);
+//    m_stackedLayout->addWidget(m_restorePage);
+
+//    mainLayout->addLayout(m_stackedLayout);
+
+
+    QTabWidget *tabWidget = new QTabWidget;
+    tabWidget->addTab(m_backupPage, tr("Backup"));
+    tabWidget->addTab(m_restorePage, tr("Restore"));
+    mainLayout->addWidget(tabWidget);
+
 
     setLayout(mainLayout);
 
-    connect(m_buttonBox, &DButtonBox::buttonClicked, this, [=] (QAbstractButton *button) {
-        const QMap<QAbstractButton*, QWidget*> page {
-            {backupBtn, m_backupPage},
-            {restoreBtn, m_restorePage},
-        };
+//    connect(m_buttonBox, &DButtonBox::buttonClicked, this, [=] (QAbstractButton *button) {
+//        const QMap<QAbstractButton*, QWidget*> page {
+//            {backupBtn, m_backupPage},
+//            {restoreBtn, m_restorePage},
+//        };
 
-        m_backupPage->setTipsVisible(false);
-        m_restorePage->setTipsVisible(false);
-        m_stackedLayout->setCurrentWidget(page[button]);
-    });
+//        m_backupPage->setTipsVisible(false);
+//        m_restorePage->setTipsVisible(false);
+//        m_stackedLayout->setCurrentWidget(page[button]);
+//    });
 
-    backupBtn->setChecked(true);
+//    backupBtn->setChecked(true);
 
     connect(m_backupPage, &ManualBackup::requestSetManualBackupDirectory, this, &SystemRestore::requestSetManualBackupDirectory);
     connect(m_backupPage, &ManualBackup::requestSetSystemBackupDirectory, this, &SystemRestore::requestSetSystemBackupDirectory);
