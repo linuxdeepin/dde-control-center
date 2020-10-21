98906e58e844408f5191ee757c834b030585e617
lxz <justforlxz@gmail.com>
2020-10-21 17:06:56 +0800

    perf: change init order
    use QFuture to load search data
    
    Log:
    Task: https://pms.uniontech.com/zentao/task-view-42119.html
    Change-Id: I54184715172bb43be7b630d4a3f79537c17546c3

--- src/frame/window/search/searchwidget.h
+++ src/frame/window/search/searchwidget.h
@@ -52,6 +52,23 @@ using WM = com::deepin::wm;
 
 namespace DCC_NAMESPACE {
 namespace search {
+struct SearchBoxStruct {
+    QString translateContent;
+    QString actualModuleName;
+    QString childPageName;
+    QString fullPagePath;
+};
+
+struct SearchDataStruct {
+    QString chiese;
+    QString pinyin;
+};
+
+struct UnexsitStruct {
+    QString module;
+    QString datail;
+};
+
 class DCompleterStyledItemDelegate : public QStyledItemDelegate
 {
     Q_OBJECT
@@ -61,27 +78,62 @@ public:
     QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 };
 
+class SearchModel : public QStandardItemModel {
+    Q_OBJECT
+    friend class SearchWidget;
+public:
+    explicit SearchModel(QObject* parent = nullptr);
+
+public:
+    bool jumpContentPathWidget(const QString &path);
+    void setLanguage(const QString &type);
+    void addModulesName(QString moduleName, const QString &searchName, QIcon icon, QString translation = "");
+    void addUnExsitData(const QString &module = "", const QString &datail = "");
+    void removeUnExsitData(const QString &module = "", const QString &datail = "");
+    void setRemoveableDeviceStatus(const QString &name, bool isExist);
+
+Q_SIGNALS:
+    void notifyModuleSearch(QString, QString);
+
+private:
+    void loadxml();
+    SearchBoxStruct getModuleBtnString(QString value);
+    QString getModulesName(const QString &name, bool state = true);
+    QString removeDigital(QString input);
+    QString transPinyinToChinese(const QString &pinyin);
+    QString containTxtData(QString txt);
+    void appendChineseData(SearchBoxStruct data);
+    bool isLoadText(const QString &txt);
+    bool isLoadContensText(const QString &text);
+
+private:
+    QList<SearchBoxStruct> m_EnterNewPagelist;
+    QString m_xmlExplain;
+    QSet<QString> m_xmlFilePath;
+    QString m_lang;
+    QMap<QString, QIcon> m_iconMap;
+    QList<QPair<QString, QString>> m_moduleNameList;//用于存储如 "update"和"Update"
+    QList<SearchDataStruct> m_inputList;
+    QList<UnexsitStruct>    m_unexsitList;
+    QList<QPair<QString, bool>> m_serverTxtList;//QString表示和服务器/桌面版有关的文言,bool:true表示只有服务器版会存在,false表示只有桌面版存在
+    QList<QString> m_TxtList;
+    QList<QString> m_TxtListAll;
+    QStringList m_defaultRemoveableList;//存储已知全部模块是否存在
+    QList<QPair<QString, QString>> m_removedefaultWidgetList;//用于存储可以出设备名称，和该名称对应的页面
+    QList<QPair<QString, QString>> m_removeableActualExistList;//存储实际模块是否存在
+    QList<QPair<QString, bool>> m_contensServerTxtList;
+    bool m_bIsChinese;
+    bool m_bIstextEdited;
+    bool m_bIsServerType;
+    bool m_bIsContensServerType;
+    QGSettings *m_searchModuleDevelop{nullptr};
+    WM *m_deepinwm;
+    QFutureWatcher<void> *m_searchWatcher;
+};
+
 class SearchWidget : public DTK_WIDGET_NAMESPACE::DSearchEdit
 {
     Q_OBJECT
-public:
-    struct SearchBoxStruct {
-        QString translateContent;
-        QString actualModuleName;
-        QString childPageName;
-        QString fullPagePath;
-    };
-
-    struct SearchDataStruct {
-        QString chiese;
-        QString pinyin;
-    };
-
-    struct UnexsitStruct {
-        QString module;
-        QString datail;
-    };
-
 public:
     SearchWidget(QWidget *parent = nullptr);
     ~SearchWidget() override;
@@ -100,46 +152,11 @@ Q_SIGNALS:
     void notifyModuleSearch(QString, QString);
 
 private:
-    void loadxml();
-    SearchBoxStruct getModuleBtnString(QString value);
-    QString getModulesName(const QString &name, bool state = true);
-    QString removeDigital(QString input);
-    QString transPinyinToChinese(const QString &pinyin);
-    QString containTxtData(QString txt);
-    void appendChineseData(SearchBoxStruct data);
-    void clearSearchData();
-    bool isLoadText(const QString &txt);
-    bool isLoadContensText(const QString &text);
-
-private:
-    QStandardItemModel *m_model;
+    SearchModel *m_model;
     QCompleter *m_completer;
-    QList<SearchBoxStruct> m_EnterNewPagelist;
-    SearchBoxStruct m_searchBoxStruct;
-    QString m_xmlExplain;
-    QSet<QString> m_xmlFilePath;
-    QString m_lang;
-    QMap<QString, QIcon> m_iconMap;
-    QList<QPair<QString, QString>> m_moduleNameList;//用于存储如 "update"和"Update"
-    QList<SearchDataStruct> m_inputList;
-    bool m_bIsChinese;
-    QList<UnexsitStruct> m_unexsitList;
     QString m_searchValue;
-    bool m_bIstextEdited;
-    QStringList m_defaultRemoveableList;//存储已知全部模块是否存在
-    QList<QPair<QString, QString>> m_removedefaultWidgetList;//用于存储可以出设备名称，和该名称对应的页面
-    QList<QPair<QString, QString>> m_removeableActualExistList;//存储实际模块是否存在
-    bool m_bIsServerType;
-    QList<QPair<QString, bool>> m_serverTxtList;//QString表示和服务器/桌面版有关的文言,bool:true表示只有服务器版会存在,false表示只有桌面版存在
-    QList<QString> m_TxtList;
-    QList<QString> m_TxtListAll;
     bool m_speechState;
     DCompleterStyledItemDelegate styledItemDelegate;
-    QList<QPair<QString, bool>> m_contensServerTxtList;
-    bool m_bIsContensServerType;
-    QGSettings *m_searchModuleDevelop{nullptr};
-    WM *m_deepinwm;
-
 };
 
 }// namespace search
