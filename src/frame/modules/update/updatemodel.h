19ff23d1da9b6e7ba017b2456a424ded3b039d1e
Zhang Qipeng <zhangqipeng@uniontech.com>
2020-11-23 10:55:52 +0800

    feat: change update page rules
    支持单独更新系统更新、应用更新和安全更新。
    
    Log: 支持单独更新系统更新、应用更新和安全更新。
    Task: https://pms.uniontech.com/zentao/task-view-49194.html
    Change-Id: I149aacdeb4c725601adf5834c61f7a54d6538752

--- src/frame/modules/update/updatemodel.h
+++ src/frame/modules/update/updatemodel.h
@@ -126,8 +126,21 @@ public:
     bool netselectExist() const;
     void setNetselectExist(bool netselectExist);
 
-    inline bool autoCheckUpdates() { return m_autoCheckUpdates; }
+    inline bool autoCheckUpdates() const { return m_autoCheckUpdates; }
     void setAutoCheckUpdates(bool autoCheckUpdates);
+
+    inline quint64 updateMode() const { return m_updateMode; }
+    void setUpdateMode(quint64 updateMode);
+
+    inline bool autoCheckSecureUpdates() const { return m_autoCheckSecureUpdates; }
+    void setAutoCheckSecureUpdates(bool autoCheckSecureUpdates);
+
+    inline bool autoCheckSystemUpdates() const { return m_autoCheckSystemUpdates; }
+    void setAutoCheckSystemUpdates(bool autoCheckSystemUpdates);
+
+    inline bool autoCheckAppUpdates() const { return m_autoCheckAppUpdates; }
+    void setAutoCheckAppUpdates(bool autoCheckAppUpdates);
+
     bool smartMirrorSwitch() const { return m_smartMirrorSwitch; }
     void setSmartMirrorSwitch(bool smartMirrorSwitch);
 
@@ -185,6 +198,9 @@ Q_SIGNALS:
     void autoCleanCacheChanged(const bool autoCleanCache);
     void netselectExistChanged(const bool netselectExist);
     void autoCheckUpdatesChanged(const bool autoCheckUpdates);
+    void autoCheckSystemUpdatesChanged(const bool autoCheckSystemUpdate);
+    void autoCheckAppUpdatesChanged(const bool autoCheckAppUpdate);
+    void autoCheckSecureUpdatesChanged(const bool autoCheckSecureUpdate);
     void recoverBackingUpChanged(bool recoverBackingUp);
     void recoverConfigValidChanged(bool recoverConfigValid);
     void recoverRestoringChanged(bool recoverRestoring);
@@ -213,6 +229,10 @@ private:
     bool m_autoCleanCache;
     bool m_autoDownloadUpdates;
     bool m_autoCheckUpdates;
+    quint64 m_updateMode;
+    bool m_autoCheckSecureUpdates;
+    bool m_autoCheckSystemUpdates;
+    bool m_autoCheckAppUpdates;
     bool m_updateNotify;
     bool m_smartMirrorSwitch;
     QString m_mirrorId;
