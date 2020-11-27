45475d522be013193621456c5943cbdfaf5d508d
justforlxz <justforlxz@gmail.com>
2020-11-27 17:26:44 +0800

    fix(bluetooth): multi thread error
    Adapter threads are inconsistent
    
    Log:
    Bug: https://pms.uniontech.com/zentao/bug-view-56138.html
    Bug: https://pms.uniontech.com/zentao/bug-view-56137.html

--- src/frame/modules/bluetooth/bluetoothworker.cpp
+++ src/frame/modules/bluetooth/bluetoothworker.cpp
@@ -402,7 +402,7 @@ void BluetoothWorker::refresh()
         QJsonDocument doc = QJsonDocument::fromJson(replyStr.toUtf8());
         QJsonArray arr = doc.array();
         for (QJsonValue val : arr) {
-            Adapter *adapter = new Adapter(m_model);
+            Adapter *adapter = new Adapter;
             inflateAdapter(adapter, val.toObject());
 
             m_model->addAdapter(adapter);
