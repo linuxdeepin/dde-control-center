2d7eebb301124bc12b495a96c5022ae9f70b6fdc
xiaoyaobing <xiaoyaobing@uniontech.com>
2020-12-03 09:19:46 +0800

    feat: 控制中心一级模块插件示例privacy
    增加控制中心一级模块插件示例,示范插件加载,翻译文件加载,UI界面编写
    
    Log: 创建控制中心一级模块插件示例privacy
    Task: https://pms.uniontech.com/zentao/task-view-44501.html
    Change-Id: Ic825690b4710c0b6d0a5c21bccef2cada36e971c

---
+++ src/frame/plugins/privacy/translations/privacy-plugin.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>LocationWidget</name>
+    <message>
+        <location filename="../locationwidget.cpp" line="29"/>
+        <source>Location service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../locationwidget.cpp" line="35"/>
+        <source>Location services use GPS, Bluetooth and WiFi hotspots and signals to determine your approximate location</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PrivacyPlugin</name>
+    <message>
+        <location filename="../privacyplugin.cpp" line="49"/>
+        <source>Privacy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PrivacyWidget</name>
+    <message>
+        <location filename="../privacywidget.cpp" line="29"/>
+        <source>Location service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../privacywidget.cpp" line="30"/>
+        <source>Photo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../privacywidget.cpp" line="31"/>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../privacywidget.cpp" line="32"/>
+        <source>Microphone</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
