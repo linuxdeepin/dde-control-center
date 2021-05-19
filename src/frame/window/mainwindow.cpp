56dd75c17f48c2f81267549dc1a655855c132328
lvpeilong <lvpeilong@uniontech.com>
2021-05-19 23:53:15 +0800

    Revert "fix: 在性能较低机器上可能出现一级菜单频繁快速切换至控制中心闪退问题"
    This reverts commit beb46264ead7670b1e8f09681554ea36a017420d.
    
    Reason for revert: 引入其它问题 暂时回退
    
    Change-Id: I7a231f8471fd3cfee617749ef84c2bde105e8b29

--- src/frame/window/mainwindow.cpp
+++ src/frame/window/mainwindow.cpp
@@ -476,10 +476,7 @@ void MainWindow::modulePreInitialize(const QString &m)
 void MainWindow::popWidget()
 {
     if (m_topWidget) {
-        //由于多个模块在性能优化或功能实现上使用singleShot，可能导致无效指针问题，在popWidget中延后删除指针
-        QTimer::singleShot(0, this, [=] {
-            m_topWidget->deleteLater();
-        });
+        m_topWidget->deleteLater();
         m_topWidget = nullptr;
         return;
     }
@@ -490,10 +487,7 @@ void MainWindow::popWidget()
 
     m_rightContentLayout->removeWidget(w);
     w->setParent(nullptr);
-    //由于多个模块在性能优化或功能实现上使用singleShot，可能导致无效指针问题，在popWidget中延后删除指针
-    QTimer::singleShot(0, this, [=] {
-        w->deleteLater();
-    });
+    w->deleteLater();
 
     //delete replace widget : first delete replace widget(up code) , then pass pushWidget to set last widget
     if (m_lastThirdPage.second) {
