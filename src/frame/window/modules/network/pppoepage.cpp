5854825a9beb9547e02af409f615714f98907879
dongrui <dongrui@uniontech.com>
2021-05-20 11:02:00 +0800

    Revert "fix: 编辑DSL网络后点保存控制中心会崩溃"
    This reverts commit 4222e35c03e99f7a94021caf50e6544dacdf00fb.
    
    Reason for revert: commit revert
    
    Change-Id: I5e93118734d3ad414bc5032409a1adeb25055431

--- src/frame/window/modules/network/pppoepage.cpp
+++ src/frame/window/modules/network/pppoepage.cpp
@@ -149,7 +149,6 @@ void PppoePage::onConnectionListChanged()
 void PppoePage::onConnectionDetailClicked(const QString &connectionUuid)
 {
     m_editPage = new ConnectionEditPage(ConnectionEditPage::ConnectionType::PppoeConnection, "/", connectionUuid);
-    m_editPage->setModel(m_model);
     m_editPage->initSettingsWidget();
     connect(m_editPage, &ConnectionEditPage::requestNextPage, this, &PppoePage::requestNextPage);
     connect(m_editPage, &ConnectionEditPage::requestFrameAutoHide, this, &PppoePage::requestFrameKeepAutoHide);
