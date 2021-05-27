a18b5cc78212778c6216dfe2f0691795dcd68849
shaotianlu <shaotianlu@uniontech.com>
2021-05-27 17:58:34 +0800

    chore: 添加内存泄露检测的编译参数
    Description: 为后面CI自动化做准备
    
    Log: 添加内存泄露检测的编译参数,输出检测文件
    Change-Id: I234dab122776a3c4497b67ba245cf1338466df7f

--- tests/test-recoverage-qmake.sh
+++ tests/test-recoverage-qmake.sh
@@ -8,3 +8,15 @@ cd $BUILD_DIR
 cmake ..
 make -j16
 make check
+
+cd tests/
+
+# dccwidgets
+mv ./dccwidgets/asan.log* ./dccwidgets/asan_dccwidgets.log
+
+#dde-control-center
+mv ./dde-control-center/asan_bluetooth.log* ./dde-control-center/asan_bluetooth.log
+mv ./dde-control-center/asan_datetime.log* ./dde-control-center/asan_datetime.log
+mv ./dde-control-center/asan_defapp.log* ./dde-control-center/asan_defapp.log
+mv ./dde-control-center/asan_mouse.log* ./dde-control-center/asan_mouse.log
+mv ./dde-control-center/asan_notification.log* ./dde-control-center/asan_notification.log
