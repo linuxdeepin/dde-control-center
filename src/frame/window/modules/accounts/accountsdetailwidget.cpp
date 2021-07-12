9b7485030e1793e95f650f428aa55e2898c35596
Tianlu Shao <shaotianlu@uniontech.com>
2021-07-12 15:27:33 +0800

    fix: 密码有效期可输入字符不符合需求
    未对首位’0‘坐判断，【“+”,可无限输入99999999】已经在之前提交中修复
    
    Log: 密码有效期可输入字符不符合需求
    Bug: https://pms.uniontech.com/zentao/bug-view-86978.html
    Change-Id: I6ca769b8a73f69eb43076898039c459089da71ea

--- src/frame/window/modules/accounts/accountsdetailwidget.cpp
+++ src/frame/window/modules/accounts/accountsdetailwidget.cpp
@@ -395,7 +395,7 @@ void AccountsDetailWidget::initSetting(QVBoxLayout *layout)
 
     pwHLayout->addWidget(new QLabel(tr("Validity Days")), 0, Qt::AlignLeft);
     auto validityDaysBox = new AccountSpinBox();
-    validityDaysBox->lineEdit()->setValidator(new QRegularExpressionValidator(QRegularExpression("0|[1-9]\\d{0,4}"), validityDaysBox->lineEdit()));
+    validityDaysBox->lineEdit()->setValidator(new QRegularExpressionValidator(QRegularExpression("[1-9]\\d{0,4}/^[1-9]\\d*$/"), validityDaysBox->lineEdit()));
     validityDaysBox->setFixedWidth(180);
     validityDaysBox->lineEdit()->setFixedWidth(80);
     validityDaysBox->lineEdit()->setPlaceholderText("99999");
