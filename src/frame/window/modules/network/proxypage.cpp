16a4e7b08b39a8516a64c4d9c1dd9ba1f83c65a8
ut000168 <wubowen@uniontech.com>
2021-08-13 22:07:12 +0800

    fix: 解决系统代理页面点击取消后再点击无法保存的问题
    系统代理页面的输入框，点击应该激活下面的两个按钮，但是点了一次取消，在点击输入框后就不能激活下面按钮了
    
    Log: 解决系统代理页面点取消后再点击输入框无法激活按钮
    Bug: https://pms.uniontech.com/zentao/bug-view-90086.html
    Change-Id: Ib4e0d51fae74d4e0699d8ecb864dae962fe81631

--- src/frame/window/modules/network/proxypage.cpp
+++ src/frame/window/modules/network/proxypage.cpp
@@ -207,7 +207,9 @@ ProxyPage::ProxyPage(QWidget *parent)
     // 取消、确定按钮响应
     connect(m_buttonTuple->rightButton(), &QPushButton::clicked, this, &ProxyPage::applySettings);
     connect(m_buttonTuple->leftButton(), &QPushButton::clicked, this, [this] {
+        m_autoUrl->dTextEdit()->clearFocus();
         m_buttonTuple->setEnabled(false);
+        m_buttonTuple->setFocus();
         if (m_proxyTypeBox->comboBox()->currentIndex() == ProxyMethodList.indexOf(MANUAL)) {
             onProxyChanged("http", m_model->proxy("http"));
             onProxyChanged("https", m_model->proxy("https"));
@@ -323,7 +325,6 @@ bool ProxyPage::eventFilter(QObject *watched, QEvent *event)
     if (event->type() == QEvent::FocusIn) {
         if ((dynamic_cast<QLineEdit*>(watched) || dynamic_cast<DTextEdit*>(watched))) {
             m_buttonTuple->setEnabled(true);
-            m_buttonTuple->setFocus();
         }
     }
     return QWidget::eventFilter(watched, event);
