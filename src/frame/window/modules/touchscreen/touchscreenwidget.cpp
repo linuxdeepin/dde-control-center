3782c11b2e53b5fd4e83933f451bbee7f82cdb69
zsien <i@zsien.cn>
2021-10-12 16:23:19 +0800

    fix(touchscreen): 触摸屏数量大于显示器数量时崩溃
    使用触摸屏数量索引显示器，导致当触摸屏数量大于显示器数量时
    出现空指针。
    
    Log: 修复点击触摸屏崩溃
    Bug: https://pms.uniontech.com/zentao/bug-view-97893.html
    Change-Id: I669ec8b2a6b322f3cb20c0d6c20919fbe34db6bb

--- src/frame/window/modules/touchscreen/touchscreenwidget.cpp
+++ src/frame/window/modules/touchscreen/touchscreenwidget.cpp
@@ -116,7 +116,8 @@ void TouchscreenWidget::onMonitorChanged()
     TouchscreenMap touchMap = m_model->touchMap();
     TouchscreenInfoList touchscreenList = m_model->touchscreenList();
     QList<Monitor *> monitorList = m_model->monitorList();
-    int cnt = 0;
+    int idx = 0;
+    int monitorMaxIdx = monitorList.count() - 1;
     for (const auto &i : touchscreenList) {
         QString touchscreenSerial = i.serialNumber;
 
@@ -133,8 +134,9 @@ void TouchscreenWidget::onMonitorChanged()
         }
 
         // 第一次开机 touchMap 为空, 会设置默认值
-        if (touchMap.isEmpty())
-            listCombo->setCurrentText(monitorList.at(cnt)->name());
+        if (touchMap.isEmpty()) {
+            listCombo->setCurrentIndex(idx > monitorMaxIdx ? monitorMaxIdx : idx);
+        }
 
         if (touchMap.find(touchscreenSerial) != touchMap.end()) {
             listCombo->setCurrentText(touchMap.value(touchscreenSerial));
@@ -159,7 +161,7 @@ void TouchscreenWidget::onMonitorChanged()
         connect(listCombo, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), [this] {
             m_buttonTuple->rightButton()->setEnabled(true);
         });
-        cnt++;
+        idx++;
     }
 }
 
