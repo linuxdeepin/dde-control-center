88c2ed8f35ba69f62d97bbeeb7ff4d5c1da61210
caixiangrong <caixiangrong@uniontech.com>
2021-10-12 10:06:51 +0800

    chore: 更新翻译脚本
    将dde-control-center_en_US.ts 改为 dde-control-center_en.ts
    从CMakeLists.txt提取要翻译的文件，避免ts文件中有不必要的翻译
    
    Log:
    Change-Id: I2973b60c8157defe22ba02c75dad5c43c0c4d887

--- lupdate.sh
+++ lupdate.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 cd ./translations
-rm -f dde-control-center_en_US.ts
-lupdate ../src/ -ts -no-obsolete dde-control-center_en_US.ts
-sed -e 's/DCC_NAMESPACE/dccV20/g' dde-control-center_en_US.ts > tmp.ts
-mv tmp.ts dde-control-center_en_US.ts
+rm -f dde-control-center_en.ts
+# files.tmp 参加翻译的文件列表，从CMakeLists中提取.h和.cpp文件
+rm -f files.tmp
+for i in $(grep "\.[h|cpp]" ../src/frame/CMakeLists.txt)
+do
+	echo ../src/frame/$i >> files.tmp
+done
+
+lupdate @files.tmp -ts -no-obsolete dde-control-center_en.ts
+rm -f files.tmp
+
+sed -e 's/DCC_NAMESPACE/dccV20/g' dde-control-center_en.ts > tmp.ts
+mv tmp.ts dde-control-center_en.ts
 cd ../
 
 tx push -s -b m20
