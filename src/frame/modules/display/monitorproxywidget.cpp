aa250a010c57254a220d9a14fee16e7c8881f663
Tianlu Shao <shaotianlu@uniontech.com>
2021-11-01 15:23:28 +0800

    fix: 修复多屏示意图标识为'0'时的遮挡问题
    当数字为0时，计算字符串的宽度偏小，导致字符存在字符遮挡
    
    Log: 修复多屏标识遮挡的问题
    Bug: https://pms.uniontech.com/zentao/task-view-90091.html
    Influence: 观察多屏显示标识尾数为0的显示情况
    Change-Id: I60e08766904d93de51d275c0a03ad0760637c9da

--- src/frame/modules/display/monitorproxywidget.cpp
+++ src/frame/modules/display/monitorproxywidget.cpp
@@ -87,7 +87,9 @@ void MonitorProxyWidget::paint(QPainter *painter, const QStyleOptionGraphicsItem
     QFont font ("Microsoft YaHei", 100, 20);
     painter->setFont(font);
     const QFontMetrics fm(painter->font());
-    const int width = fm.boundingRect(name()).width();
+    
+    //修复在数字为0的时候，计算的字体宽度偏小导致显示遮挡的问题
+    const int width = fm.boundingRect(name()).width() + 100;
     const int height = fm.boundingRect(name()).height();
     painter->setPen(Qt::white);
     if (m_model->displayMode() != MERGE_MODE) {
