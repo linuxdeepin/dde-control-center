5c19ac3c16cd91f41870e7b4a350c95438975552
wang fei <wangfeia@uniontech.com>
2022-01-06 14:37:18 +0800

    fix: 修改翻译脚本
    翻译脚本lupdate 添加重置密码模块
    
    Log: 修改翻译脚本
    Bug: https://pms.uniontech.com/zentao/bug-view-108234.html
    Influence: 翻译
    Change-Id: Ib8dac975823690ca648f6c31c3c470f3359af4c9

--- lupdate.sh
+++ lupdate.sh
@@ -20,4 +20,26 @@ sed -e 's/DCC_NAMESPACE/dccV20/g' dde-control-center_en.ts > tmp.ts
 mv tmp.ts dde-control-center_en.ts
 cd ../
 
+# reset-pasword-dialog
+cd ./translations
+rm -f reset-password-dialog_en.ts
+# files.tmp 参加翻译的文件列表，从CMakeLists中提取.h和.cpp文件
+rm -f files.tmp
+for i in $(grep "\.h" ../src/reset-password-dialog/CMakeLists.txt)
+do
+	echo ../src/reset-password-dialog/$i >> files.tmp
+done
+
+for i in $(grep "\.cpp" ../src/reset-password-dialog/CMakeLists.txt)
+do
+	echo ../src/reset-password-dialog/$i >> files.tmp
+done
+
+lupdate @files.tmp -I ../include/ -ts -no-obsolete reset-password-dialog_en.ts
+rm -f files.tmp
+
+sed -e 's/DCC_NAMESPACE/dccV20/g' reset-password-dialog_en.ts > tmp.ts
+mv tmp.ts reset-password-dialog_en.ts
+cd ../
+
 tx push -s -b m20
