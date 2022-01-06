46a80f28684e48f58a34586278146e0d1954be65
caixiangrong <caixiangrong@uniontech.com>
2022-01-06 17:17:56 +0800

    fix: 修改翻译脚本增加所有控制中心项目
    翻译脚本lupdate增加所有控制中心项目
    
    Log: 修改翻译脚本
    Bug: https://pms.uniontech.com/zentao/task-view-97193.html
    Influence: 翻译
    Change-Id: Ib0818b3db5f7e11299df7160ea79f302f0f1610d

--- lupdate.sh
+++ lupdate.sh
@@ -1,45 +1,56 @@
 #!/bin/bash
-cd ./translations
-rm -f dde-control-center_en.ts
-# files.tmp 参加翻译的文件列表，从CMakeLists中提取.h和.cpp文件
-rm -f files.tmp
-for i in $(grep "\.h" ../src/frame/CMakeLists.txt)
-do
-	echo ../src/frame/$i >> files.tmp
-done
 
-for i in $(grep "\.cpp" ../src/frame/CMakeLists.txt)
-do
-	echo ../src/frame/$i >> files.tmp
-done
+# 根据CMakeLists.txt更新ts文件
+# 参数1 ts文件名，不带_en.ts
+# 参数2 CMakeLists.txt相对路径
+# 参数3 附加头文件路径，可无
+function lupdateProject() {
+	local INC_FLAG=
+	local SRC_DIR=$2
+	local TS_FILE=$1_en.ts
+	echo 工程:$1
+	echo CMakeLists.txt路径:$2
+	if [ $# == 3 ];then
+		echo 附加头文件路径:$3
+		INC_FLAG=-I\ $3
+	fi
+	cd ./translations
+	rm -f $TS_FILE
+	rm -f files.tmp
+	for i in $(grep "\.h" $SRC_DIR/CMakeLists.txt)
+	do
+		echo $SRC_DIR/$i >> files.tmp
+	done
 
-lupdate @files.tmp -I ../include/ -ts -no-obsolete dde-control-center_en.ts
-rm -f files.tmp
+	for i in $(grep "\.cpp" $SRC_DIR/CMakeLists.txt)
+	do
+		echo $SRC_DIR/$i >> files.tmp
+	done
+	lupdate @files.tmp $INC_FLAG -ts -no-obsolete $TS_FILE
+	rm -f files.tmp
 
-sed -e 's/DCC_NAMESPACE/dccV20/g' dde-control-center_en.ts > tmp.ts
-mv tmp.ts dde-control-center_en.ts
-cd ../
+	sed -e 's/DCC_NAMESPACE/dccV20/g' $TS_FILE > tmp.ts
+	mv tmp.ts $TS_FILE
+	cd ../
+}
+
+
+
+# dde-control-center_en.ts
+lupdateProject dde-control-center ../src/frame ../include
+
+# dialogs_en.ts
+lupdateProject dialogs ../src/reboot-reminder-dialog
+
+# desktop.ts  无源文件
+
+# recovery_en.ts
+lupdateProject recovery ../abrecovery
+
+# keyboard_language_en.ts
+# keyboard_language 无源文件，直接修改keyboard_language_en.ts
 
 # reset-pasword-dialog
-cd ./translations
-rm -f reset-password-dialog_en.ts
-# files.tmp 参加翻译的文件列表，从CMakeLists中提取.h和.cpp文件
-rm -f files.tmp
-for i in $(grep "\.h" ../src/reset-password-dialog/CMakeLists.txt)
-do
-	echo ../src/reset-password-dialog/$i >> files.tmp
-done
-
-for i in $(grep "\.cpp" ../src/reset-password-dialog/CMakeLists.txt)
-do
-	echo ../src/reset-password-dialog/$i >> files.tmp
-done
-
-lupdate @files.tmp -I ../include/ -ts -no-obsolete reset-password-dialog_en.ts
-rm -f files.tmp
-
-sed -e 's/DCC_NAMESPACE/dccV20/g' reset-password-dialog_en.ts > tmp.ts
-mv tmp.ts reset-password-dialog_en.ts
-cd ../
+lupdateProject reset-password-dialog ../src/reset-password-dialog
 
 tx push -s -b m20
