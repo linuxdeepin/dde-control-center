cbf2cc8268396ce20795b99f102e5111d3333776
wang fei <wangfeia@uniontech.com>
2022-02-11 17:18:47 +0800

    fix: 通过unionid重置密码,部分手机号码识别为格式错误
    更新校验的手机号码段
    
    Log: 解决通过unionid重置密码,部分手机号码识别为格式错误的问题
    Bug: https://pms.uniontech.com/zentao/bug-view-113120.html
    Influence: 手机号码格式校验
    Change-Id: I61369569922d3c864e63c6e638171eebb4b4153e

--- src/reset-password-dialog/resetpassworddialog.cpp
+++ src/reset-password-dialog/resetpassworddialog.cpp
@@ -632,7 +632,7 @@ QString ResetPasswordDialog::cryptUserPassword(const QString &password)
 
 bool ResetPasswordDialog::checkPhoneEmailFormat(const QString &content)
 {
-    QRegExp phoneRegExp("^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\\d{8}$");
+    QRegExp phoneRegExp("^1\\d{10}$");
     QRegExp emailRegExp("^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$");
     return phoneRegExp.exactMatch(content) || emailRegExp.exactMatch(content);
 }
