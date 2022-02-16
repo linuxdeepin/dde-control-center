75003e45928973c8fdaf9806d5e8322c328b6ce4
沈文骐 <shenwenqi@uniontech.com>
2022-02-16 11:01:31 +0800

    feat: 修改单元测试结果目录结构
    修改单元测试结果目录结构
    
    Log: 修改单元测试结果目录结构
    Task: https://pms.uniontech.com/zentao/task-view-100126.html
    Influence: 单元测试输出结果
    Change-Id: I725a857cd6d9933efe53f3307cedd6d987230a10

--- tests/test-recoverage-qmake.sh
+++ tests/test-recoverage-qmake.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-BUILD_DIR=build
+BUILD_DIR=build-ut
+HTML_DIR=html
 REPORT_DIR=report
 cd ../
 rm -rf $BUILD_DIR
@@ -11,7 +12,7 @@ make -j16
 cd tests/
 
 cd dccwidgets/
-./dccwidgets-unittest --gtest_output=xml:dde_test_report_dccwidgets.xml
+./dccwidgets-unittest --gtest_output=xml:ut-report_dccwidgets.xml
 # dccwidgets
 mv asan.log* asan_dccwidgets.log
 
@@ -25,14 +26,15 @@ lcov --directory ./CMakeFiles/datetime-unittest.dir --zerocounters
 #lcov --directory ./CMakeFiles/defapp-unittest.dir --zerocounters
 lcov --directory ./CMakeFiles/systeminfo-unittest.dir --zerocounters
 lcov --directory ./CMakeFiles/keyboard-unittest.dir --zerocounters
+lcov --directory ../dccwidgets/CMakeFiles/dccwidgets-unittest.dir --zerocounters
 echo " =================== Start Unit  ==================== "
 #./bluetooth-unittest --gtest_output=xml:dde_test.xml
-./mouse-unittest --gtest_output=xml:dde_test_report_mouse.xml
-./systeminfo-unittest --gtest_output=xml:dde_test_report_systeminfo.xml
-./datetime-unittest --gtest_output=xml:dde_test_report_datetime.xml
+./mouse-unittest --gtest_output=xml:../../report/ut-report_mouse.xml
+./systeminfo-unittest --gtest_output=xml:../../report/ut-report_systeminfo.xml
+./datetime-unittest --gtest_output=xml:../../report/ut-report_datetime.xml
 #./defapp-unittest --gtest_output=xml:dde_test_report_defapp.xml
 #./notification-unittest --gtest_output=xml:dde_test_report_notification.xml
-./keyboard-unittest --gtest_output=xml:dde_test_report_keyboard.xml
+./keyboard-unittest --gtest_output=xml:../../report/ut-report_keyboard.xml
 echo " =================== do filter begin ==================== "
 lcov --directory . --capture --output-file ./coverage.info
 echo " =================== get info end ==================== "
@@ -40,15 +42,18 @@ echo " =================== do filter begin ==================== "
 
 lcov --remove ./coverage.info  '*/${MOUSE_NAME}_autogen/*' '*/${NOTIFICATION_NAME}_autogen/*' '*/${DATETIME_NAME}_autogen/*' '*/${DEFAPP_NAME}_autogen/*' '*/${SYSTEMINFO_NAME}_autogen/*' '*/${KEYBOARD_NAME}_autogen/*' '*/usr/include/*' '*/tests/*' '*/googletest/*' '*/widgets/*' '*/display/*' '*/interface/*' -o ./coverage.info
 echo " =================== do filter end ==================== "
-genhtml -o ../../tests/report ./coverage.info
+genhtml -o ../../html ./coverage.info
 echo " =================== CREAT LCOV REPROT END ==================== "
-echo " Coverage files have been output to ${CMAKE_BINARY_DIR}/tests/report "
+echo " Coverage files have been output to ${CMAKE_BINARY_DIR}/html "
 
 #dde-control-center
 #mv asan_bluetooth.log* asan_bluetooth.log
-mv asan_mouse.log* asan_mouse.log
-mv asan_systeminfo.log* asan_systeminfo.log
+mv asan_mouse.log* ../../asan_mouse.log
+mv asan_systeminfo.log* ../../asan_systeminfo.log
 #mv asan_defapp.log* asan_defapp.log
-mv asan_datetime.log* .asan_datetime.log
+mv asan_datetime.log* ../../asan_datetime.log
 #mv asan_notification.log* asan_notification.log
-mv asan_keyboard.log* asan_keyboard.log
+mv asan_keyboard.log* ../../asan_keyboard.log
+
+
+mv ../../html/index.html ../../html/cov_dde-control-center.html
