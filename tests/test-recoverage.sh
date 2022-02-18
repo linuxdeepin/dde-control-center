e49d0e03afdf0e6af75cbfde0db9048d2932e287
沈文骐 <shenwenqi@uniontech.com>
2022-02-18 15:17:17 +0800

    feat: 单元测试结果目录结构调整
    修改单元测试结果目录结构
    
    Log: 单元测试结果目录结构调整
    Task: https://pms.uniontech.com/zentao/task-view-100126.html
    Influence: 单元测试输出结果
    Change-Id: Idd8c5199ccc8271fd562862dba23f59609f7a168

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-BUILD_DIR=build
+BUILD_DIR=build-ut
+HTML_DIR=html
 REPORT_DIR=report
 
+cd ../
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
+mkdir $HTML_DIR
+mkdir $REPORT_DIR
 
-cmake -DCMAKE_BUILD_TYPE=Debug ../../
 
-cmake ../../
+cmake -DCMAKE_BUILD_TYPE=Debug ../
 make -j 8
 
-cd tst-dde-network-core/
+cd tests/
 
 lcov -c -i -d ./ -o init.info
-./tst-dde-network --gtest_output=xml:dde_tst-dde-network-core.xml
+./tst-dde-network --gtest_output=xml:../$REPORT_DIR/ut-report_dde-network-core.xml
 lcov -c -d ./ -o cover.info
 lcov -a init.info -a cover.info -o total.info
-lcov -r total.info "*/tests/*" "*/usr/include*" "*build/src*" -o final.info
+lcov -r total.info ./te"*/tests/*" "*/usr/include*" "*build/src*" -o final.info
 
-rm -rf ../../$REPORT_DIR
-mkdir -p ../../$REPORT_DIR
-genhtml -o ../../$REPORT_DIR final.info
+genhtml -o ../$HTML_DIR final.info
+mv ../$HTML_DIR/index.html ../$HTML_DIR/cov_dde-network-core.html
 
-mv asan.log* asan_dde-network-core.log
+mv asan.log* ../asan_dde-network-core.log
