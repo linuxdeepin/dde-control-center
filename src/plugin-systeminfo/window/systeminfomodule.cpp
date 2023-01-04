99259bdbbb2056feca193474fc314057c6f40640
Robertkill <lvpeilong@uniontech.com>
2023-01-04 17:02:41 +0800

    chore: 修改copyright为读取当前年份
    修改copyright为读取当前年份
    
    Log: 修改copyright为读取当前年份
    Influence: 控制中心系统信息关于本机copyright年份
    Task: https://pms.uniontech.com/task-view-234481.html
    Change-Id: Ie7bbd097784cba53a40ec807944979a7efb9500c

--- src/plugin-systeminfo/window/systeminfomodule.cpp
+++ src/plugin-systeminfo/window/systeminfomodule.cpp
@@ -41,6 +41,7 @@
 #include <qtimer.h>
 #include <qapplication.h>
 #include <qwidget.h>
+#include <QDate>
 
 using namespace DCC_NAMESPACE;
 DCORE_USE_NAMESPACE
@@ -113,9 +114,9 @@ const QString systemCopyright()
     const QString &oem_copyright = settings.value("system_info_vendor_name").toString().toLatin1();
     if (oem_copyright.isEmpty()) {
         if (IS_COMMUNITY_SYSTEM)
-            return QApplication::translate("LogoModule", "Copyright© 2011-%1 Deepin Community").arg(2022);
+            return QApplication::translate("LogoModule", "Copyright© 2011-%1 Deepin Community").arg(QDate::currentDate().year());
         else
-            return QApplication::translate("LogoModule", "Copyright© 2019-%1 UnionTech Software Technology Co., LTD").arg(2022);
+            return QApplication::translate("LogoModule", "Copyright© 2019-%1 UnionTech Software Technology Co., LTD").arg(QDate::currentDate().year());
     } else {
         return oem_copyright;
     }
