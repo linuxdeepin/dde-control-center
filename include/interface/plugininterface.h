7913777e8d96e6a08ce02cccf20159c84157b6e2
chenhongtao <chenhongtao@uniontech.com>
2023-02-16 15:25:21 +0800

    chore: reuse copyright
    Log: copyright is missing

--- include/interface/plugininterface.h
+++ include/interface/plugininterface.h
@@ -1,3 +1,4 @@
+//Copyright (C) 2011 ~ 2018 Deepin Technology Co., Ltd.
 //SPDX-FileCopyrightText: 2018 - 2023 UnionTech Software Technology Co., Ltd.
 //
 //SPDX-License-Identifier: GPL-3.0-or-later
