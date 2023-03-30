776a316b2cc337d24e533dad4787565a5f51bfe8
chenhongtao <chenhongtao@uniontech.com>
2023-03-30 17:54:19 +0800

    chore: fix timeout not return
    emit finished signal will not stop wait for finished
    
    so try to use loop to check the status of watcher
    
    Log: fix timeout not return

--- src/frame/pluginmanager.h
+++ src/frame/pluginmanager.h
@@ -45,6 +45,7 @@ public Q_SLOTS:
 Q_SIGNALS:
     void loadedModule(const PluginData &data);
     void loadAllFinished();
+    void requestForceContinue();
 
 private:
     ModuleObject *findModule(ModuleObject *module, const QString &name);
