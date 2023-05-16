b792792cfee08110c7f74672a7a5afe21f291cdb
chenhongtao <chenhongtao@deepin.org>
2023-05-16 14:11:18 +0800

    chore: handle cppcheck
    Log: handle cppcheck

--- src/plugin-datetime/window/widgets/timezone.h
+++ src/plugin-datetime/window/widgets/timezone.h
@@ -1,28 +1,30 @@
-//SPDX-FileCopyrightText: 2018 - 2023 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2018 - 2023 UnionTech Software Technology Co., Ltd.
 //
-//SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 #ifndef INSTALLER_SYSINFO_TIMEZONE_H
 #define INSTALLER_SYSINFO_TIMEZONE_H
 
-#include <QList>
 #include <QHash>
+#include <QList>
 
 namespace installer {
 
-struct ZoneInfo {
- public:
-  QString country;
-  QString timezone;
+struct ZoneInfo
+{
+public:
+    QString country;
+    QString timezone;
 
-  // Coordinates of zone.
-  double latitude;
-  double longitude;
+    // Coordinates of zone.
+    double latitude;
+    double longitude;
 
-  // Distance to clicked point for comparison.
-  double distance;
+    // Distance to clicked point for comparison.
+    double distance;
 };
-bool ZoneInfoDistanceComp(const ZoneInfo& a, const ZoneInfo& b);
-QDebug& operator<<(QDebug& debug, const ZoneInfo& info);
+
+[[maybe_unused]] bool ZoneInfoDistanceComp(const ZoneInfo &a, const ZoneInfo &b);
+QDebug &operator<<(QDebug &debug, const ZoneInfo &info);
 typedef QList<ZoneInfo> ZoneInfoList;
 
 // Read available timezone info in zone.tab file.
@@ -30,35 +32,36 @@ ZoneInfoList GetZoneInfoList();
 
 // Find ZoneInfo based on |country| or |timezone|.
 // Returns -1 if not found.
-int GetZoneInfoByCountry(const ZoneInfoList& list, const QString& country);
-int GetZoneInfoByZone(const ZoneInfoList& list, const QString& timezone);
+[[maybe_unused]] int GetZoneInfoByCountry(const ZoneInfoList &list, const QString &country);
+int GetZoneInfoByZone(const ZoneInfoList &list, const QString &timezone);
 
 // Read current timezone in /etc/timezone file.
 QString GetCurrentTimezone();
 
 // Returns name of timezone, excluding continent name.
-QString GetTimezoneName(const QString& timezone);
+[[maybe_unused]] QString GetTimezoneName(const QString &timezone);
 
 // Returns local name of timezone, excluding continent name.
 // |locale| is desired locale name.
-QString GetLocalTimezoneName(const QString& timezone, const QString& locale);
+QString GetLocalTimezoneName(const QString &timezone, const QString &locale);
 
 // A map between old name of timezone and current name.
 // e.g. Asia/Chongqing -> Asia/Shanghai
 typedef QHash<QString, QString> TimezoneAliasMap;
-TimezoneAliasMap GetTimezoneAliasMap();
+[[maybe_unused]] TimezoneAliasMap GetTimezoneAliasMap();
 
 // Validate |timezone|.
-bool IsValidTimezone(const QString& timezone);
+[[maybe_unused]] bool IsValidTimezone(const QString &timezone);
 
-struct TimezoneOffset {
-  QString name;  // Offset name, like CST.
-  long seconds;  // Offset seconds.
+struct TimezoneOffset
+{
+    QString name; // Offset name, like CST.
+    long seconds; // Offset seconds.
 };
 
 // Get |timezone| GMT offset.
-TimezoneOffset GetTimezoneOffset(const QString& timezone);
+[[maybe_unused]] TimezoneOffset GetTimezoneOffset(const QString &timezone);
 
-}  // namespace installer
+} // namespace installer
 
-#endif  // INSTALLER_SYSINFO_TIMEZONE_H
+#endif // INSTALLER_SYSINFO_TIMEZONE_H
