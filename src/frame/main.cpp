85cb4f96f7b3f395c157f48e96e69f10e7babab1
chenhongtao <chenhongtao@deepin.org>
2023-09-11 10:14:41 +0800

    chore: resize mainwindow at first
    Issue: https://github.com/linuxdeepin/developer-center/issues/4253
    Log:

--- src/frame/main.cpp
+++ src/frame/main.cpp
@@ -19,6 +19,8 @@
 DWIDGET_USE_NAMESPACE
 DCORE_USE_NAMESPACE
 
+constexpr QSize MainWindowStartSize(QSize(1000, 600));
+
 QStringList defaultpath()
 {
     QStringList path;
@@ -123,6 +125,7 @@ int main(int argc, char *argv[])
     QAccessible::installFactory(accessibleFactory);
 
     DCC_NAMESPACE::MainWindow mw;
+    mw.resize(MainWindowStartSize);
 
     DCC_NAMESPACE::ControlCenterDBusAdaptor adaptor(&mw);
     DCC_NAMESPACE::DBusControlCenterGrandSearchService grandSearchadAptor(&mw);
