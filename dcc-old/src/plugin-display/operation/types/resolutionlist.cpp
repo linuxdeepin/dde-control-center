02b4b122772156d5e6c26cdd939a88e14a6a3df2
caixiangrong <caixiangrong@uniontech.com>
2024-08-30 16:23:24 +0800

    feat: Dealing with the coexistence of old and new control centers
    Dealing with the coexistence of old and new control centers
    
    Log:

---
+++ dcc-old/src/plugin-display/operation/types/resolutionlist.cpp
@@ -0,0 +1,12 @@
+//SPDX-FileCopyrightText: 2018 - 2023 UnionTech Software Technology Co., Ltd.
+//
+//SPDX-License-Identifier: GPL-3.0-or-later
+#include "resolutionlist.h"
+
+void registerResolutionListMetaType()
+{
+    registerResolutionMetaType();
+
+    qRegisterMetaType<ResolutionList>("ResolutionList");
+    qDBusRegisterMetaType<ResolutionList>();
+}
