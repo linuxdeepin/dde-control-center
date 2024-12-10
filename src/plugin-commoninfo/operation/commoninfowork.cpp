cd1d74c0b249e83b6fd7eeed39d88563f9420460
swq <shenwenqi@uniontech.com>
2024-12-10 14:25:55 +0800

    fix: Change User DevelopMode license pop-up window to modal
    Change User DevelopMode license pop-up window to modal
    
    Log: Change User DevelopMode license pop-up window to modal
    pms: BUG-290077

--- src/plugin-commoninfo/operation/commoninfowork.cpp
+++ src/plugin-commoninfo/operation/commoninfowork.cpp
@@ -558,6 +558,7 @@ void CommonInfoWork::setEnableDeveloperMode(bool enabled)
     contentFile->write(content.toLocal8Bit());
     contentFile->close();
 
+    m_commomModel->setNeedShowModalDialog(true);
     auto pathType = "-c";
     QStringList sl;
     sl << "zh_CN" << "zh_TW";
@@ -573,6 +574,7 @@ void CommonInfoWork::setEnableDeveloperMode(bool enabled)
         } else {
             qInfo() << QString("On %1, Remove developer mode Disclaimer!").arg(current_date);
         }
+        m_commomModel->setNeedShowModalDialog(false);
         contentFile->remove();
         contentFile->deleteLater();
         m_process->deleteLater();
