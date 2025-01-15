54026bdfcd2f394e13254fb8ddc8675e47a643a5
沈文骐 <50741705+swq11@users.noreply.github.com>
2025-01-15 09:30:02 +0800

    chore(update-plugin): update translate
    as title
    
    pms: TASK-362119

--- translations/dde-control-center_nb_NO.ts
+++ translations/dde-control-center_nb_NO.ts
@@ -5505,10 +5505,6 @@ Sign in to %1 ID to get personalized features and services of Browser, App Store
 <context>
     <name>UpdateList</name>
     <message>
-        <source>本次补丁更新主要提升了桌面环境、常用应用的操作性能体验，并且修复部分系统已知问题，进一步提升了产品质量。</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Updates：</source>
         <translation type="unfinished"></translation>
     </message>
@@ -5518,6 +5514,10 @@ Sign in to %1 ID to get personalized features and services of Browser, App Store
     </message>
     <message>
         <source>View Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This patch update mainly improves the operational performance experience of desktop environment and commonly used applications, and fixes some known system issues, further enhancing product quality.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
