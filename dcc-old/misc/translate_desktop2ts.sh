a5a0e7366633a2effd478a0ed1608ecd4e6d1e1c
caixiangrong <caixiangrong@uniontech.com>
2025-12-23 14:21:49 +0800

    feat: Remove old code
    Remove old code
    
    feat: 移除旧代码
    
    移除不用的旧代码

--- dcc-old/misc/translate_desktop2ts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DESKTOP_SOURCE_FILE=org.deepin.dde.control-center.desktop
-DESKTOP_TS_DIR=../translations/desktop/
-
-/usr/bin/deepin-desktop-ts-convert desktop2ts $DESKTOP_SOURCE_FILE $DESKTOP_TS_DIR
